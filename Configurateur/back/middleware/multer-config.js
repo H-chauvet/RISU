e8af1c9a87c1ea4ea57a92096fcef5be7ca593ec
Cédric <cedric.corge@epitech.eu>
2023-04-28 22:51:45 +0200

    [ADD] database schema, initialisation of prisma in docker

--- Configurateur/back/middleware/multer-config.js
+++ Configurateur/back/middleware/multer-config.js
@@ -1,20 +1,20 @@
-const multer = require('multer');
+const multer = require('multer')
 
 const MIME_TYPES = {
   'image/jpg': 'jpg',
   'image/jpeg': 'jpg',
   'image/png': 'png'
-};
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
-    callback(null, 'images');
+    callback(null, 'images')
   },
   filename: (req, file, callback) => {
-    const name = file.originalname.split(' ').join('_');
-    const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + '.' + extension);
+    const name = file.originalname.split(' ').join('_')
+    const extension = MIME_TYPES[file.mimetype]
+    callback(null, name + Date.now() + '.' + extension)
   }
-});
+})
 
-module.exports = multer({storage: storage}).single('image');
+module.exports = multer({ storage: storage }).single('image')
