d684a17653371f050ee1c0c8b9be948bec775e36
tbellicha <tanguy.bellicha@epitech.eu>
2023-06-13 19:34:57 +0200

    [ADD] Send email verification and reset password

--- Application/server/server.js
+++ Application/server/server.js
@@ -12,6 +12,7 @@ const jwt = require('jwt-simple')
 const utils = require('./utils')
 const axios = require('axios')
 require('dotenv').config({ path: '../application.env' })
+const nodemailer = require('nodemailer')
 
 const app = express()
 
@@ -76,8 +77,9 @@ app.post('/api/signup', (req, res, next) => {
         })
         await database.prisma.User.update({
           where: { id: decoded.id },
-          data: { mailVerification: true }
+          data: { mailVerification: false }
         })
+        sendAccountConfirmationEmail(user.email, token)
       } catch (err) {
         console.error(err.message)
         res.status(401).send('An error occured.')
@@ -110,6 +112,107 @@ app.get('/api/dev/user/listall', async (req, res) => {
   } catch (err) {
     console.log(err)
     return res.status(400).json('An error occured.')
+  }
+})
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  auth: {
+    user: 'risu.epitech@gmail.com',
+    pass: 'xojjtrvrcfwnlmln'
+  }
+})
+
+async function sendResetPasswordEmail (email, newPassword) {
+  const mailOptions = {
+    from: 'risu.epitech@gmail.com',
+    to: email,
+    subject: 'Reset Your Password',
+    text: `Your new password is: ${newPassword}`
+  }
+
+  try {
+    const info = await transporter.sendMail(mailOptions)
+    console.log('Email sent to ' + email + ' (' + info.response + ')')
+  } catch (error) {
+    console.error('Error sending reset password email:', error)
+  }
+}
+
+function generateRandomPassword (length) {
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@m!$%&*?'
+  let password = ''
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length)
+    password += characters.charAt(randomIndex)
+  }
+  return password
+}
+
+app.post('/api/user/resetPassword', async (req, res) => {
+  const { email } = req.body
+
+  try {
+    const user = await database.prisma.User.findUnique({ where: { email } })
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' })
+    }
+
+    const newPassword = generateRandomPassword(8)
+
+    await database.prisma.User.update({
+      where: { id: user.id },
+      data: { password: newPassword }
+    })
+
+    await sendResetPasswordEmail(email, newPassword)
+
+    return res.status(200).json({ message: 'Reset password email sent' })
+  } catch (error) {
+    console.error('Failed to reset password:', error)
+    return res.status(500).json({ error: 'Failed to reset password' })
+  }
+})
+
+async function sendAccountConfirmationEmail (email, token) {
+  const mailOptions = {
+    from: 'risu.epitech@gmail.com',
+    to: email,
+    subject: 'Confirm your account',
+    text:
+      'Please follow the link to confirm your account: http://localhost:8080/api/mailVerification?token=' +
+      token
+  }
+
+  try {
+    const info = await transporter.sendMail(mailOptions)
+    console.log('Email sent to ' + email + ' (' + info.response + ')')
+    console.log(mailOptions.text)
+  } catch (error) {
+    console.error('Error sending reset password email:', error)
+  }
+}
+
+app.get('/api/mailVerification', async (req, res) => {
+  const token = req.query.token
+  try {
+    const decoded = jwt.decode(token, process.env.JWT_SECRET)
+    const user = await database.prisma.User.findUnique({
+      where: { id: decoded.id }
+    })
+    await database.prisma.User.update({
+      where: { id: decoded.id },
+      data: { mailVerification: true }
+    })
+    res.send(
+      'Email now successfully verified !\nYou can go back to login page.'
+    )
+  } catch (err) {
+    console.error(err.message)
+    res.status(401).send('No matching user found.')
   }
 })
 
