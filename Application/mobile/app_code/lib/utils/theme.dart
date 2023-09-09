7b9eef4dea5e5cb60a3435e115f4f1aaa461f1a4
tbellicha <tanguy.bellicha@epitech.eu>
2023-09-09 10:26:28 +0200

    [ADD] ThemeProvider class
    Signed-off-by: tbellicha <tanguy.bellicha@epitech.eu>

---
+++ Application/mobile/app_code/lib/utils/theme.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'colors.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  late ThemeData _currentTheme;
+
+  ThemeProvider(bool isDarkTheme) {
+    _currentTheme = isDarkTheme ? darkTheme : lightTheme;
+  }
+
+  ThemeData get currentTheme => _currentTheme;
+
+  void toggleTheme() async {
+    _currentTheme = _currentTheme == lightTheme ? darkTheme : lightTheme;
+    notifyListeners();
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setBool('isDarkTheme', _currentTheme == darkTheme);
+  }
+}
