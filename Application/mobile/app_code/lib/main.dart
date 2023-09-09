456e6aa95fb21cb513fd81eaa32a08f0cdca2b82
tbellicha <tanguy.bellicha@epitech.eu>
2023-09-09 10:23:24 +0200

    [ADD] Read SharedPreferences and use saved Theme
    Signed-off-by: tbellicha <tanguy.bellicha@epitech.eu>

--- Application/mobile/app_code/lib/main.dart
+++ Application/mobile/app_code/lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:risu/router.dart';
+import 'package:risu/utils/theme.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final prefs = await SharedPreferences.getInstance();
+  final isDarkTheme = prefs.getBool('isDarkTheme') ?? false;
+
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(isDarkTheme),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
