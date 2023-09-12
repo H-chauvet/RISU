79259572988f1a4735df1c5656566d35be6ccebe
tbellicha <tanguy.bellicha@epitech.eu>
2023-09-12 02:19:48 +0200

    [ADD] Some Theme colors
    Signed-off-by: tbellicha <tanguy.bellicha@epitech.eu>

--- Application/mobile/app_code/lib/utils/colors.dart
+++ Application/mobile/app_code/lib/utils/colors.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 
 class MyColors {
-  static const Color primary = Color(0xFF0F3F62);
-  static const Color secondary = Color(0xFF2996CE);
-  static const Color buttonBackground = Color(0xFFCDE8F1);
-  static const Color buttonText = Color(0xFF2996CE);
   static const Color textTitle = Color(0xFF0F3F62);
   static const Color textPrimary = Color(0xFF2996CE);
   static const Color textSecondary = Color(0xFF9898A5);
@@ -32,10 +28,82 @@ class MyColors {
 
 final lightTheme = ThemeData(
   brightness: Brightness.light,
-  primaryColor: MyColors.secondary,
+  primaryColor: const Color(0xFF4682B4),
+  secondaryHeaderColor: const Color(0xFF23415A),
+  colorScheme: const ColorScheme.light(
+    background: Color(0xFFE5E5E5),
+  ),
+  inputDecorationTheme: const InputDecorationTheme(
+    filled: false,
+    border: OutlineInputBorder(
+      borderSide: BorderSide.none,
+      borderRadius: BorderRadius.all(
+        Radius.circular(8.0),
+      ),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Color(0xFFA1C1DB),
+        width: 2.0,
+      ),
+      borderRadius: BorderRadius.all(
+        Radius.circular(8.0),
+      ),
+    ),
+    hintStyle: TextStyle(
+      color: Color(0xFF808080),
+    ),
+    labelStyle: TextStyle(
+      color: Color(0xFF000000),
+    ),
+    floatingLabelStyle: TextStyle(
+      color: Color(0xFF0F3F62),
+    ),
+  ),
+  buttonTheme: const ButtonThemeData(
+    colorScheme: ColorScheme.light(
+      secondary: Color(0xFFA1C1DB),
+    ),
+  ),
 );
 
 final darkTheme = ThemeData(
   brightness: Brightness.dark,
-  primaryColor: MyColors.primary,
+  primaryColor: const Color(0xFF23415A),
+  secondaryHeaderColor: const Color(0xFF4682B4),
+  colorScheme: const ColorScheme.dark(
+    background: Color(0xFF1E1E1E),
+  ),
+  inputDecorationTheme: const InputDecorationTheme(
+    filled: false,
+    border: OutlineInputBorder(
+      borderSide: BorderSide.none,
+      borderRadius: BorderRadius.all(
+        Radius.circular(8.0),
+      ),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Color(0xFFA1C1DB),
+        width: 2.0,
+      ),
+      borderRadius: BorderRadius.all(
+        Radius.circular(8.0),
+      ),
+    ),
+    hintStyle: TextStyle(
+      color: Color(0xFF808080),
+    ),
+    labelStyle: TextStyle(
+      color: Color(0xFFFFFFFF),
+    ),
+    floatingLabelStyle: TextStyle(
+      color: Color(0xFF4682B4),
+    ),
+  ),
+  buttonTheme: const ButtonThemeData(
+    colorScheme: ColorScheme.dark(
+      secondary: Color(0xFFA1C1DB),
+    ),
+  ),
 );
