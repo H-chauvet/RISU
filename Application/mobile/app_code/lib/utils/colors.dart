2aa687c2a339e0d4484f71427404c9dacebbcc94
tbellicha <tanguy.bellicha@epitech.eu>
2023-09-13 03:16:19 +0200

    [UPDATE] Remove useless colors and add AlertDialogTheme
    Signed-off-by: tbellicha <tanguy.bellicha@epitech.eu>

--- Application/mobile/app_code/lib/utils/colors.dart
+++ Application/mobile/app_code/lib/utils/colors.dart
@@ -1,37 +1,24 @@
 import 'package:flutter/material.dart';
 
 class MyColors {
-  static const Color textTitle = Color(0xFF0F3F62);
-  static const Color textPrimary = Color(0xFF2996CE);
-  static const Color textSecondary = Color(0xFF9898A5);
-  static const Color textDanger = Color(0xFFF64E60);
-  static const Color textSuccess = Color(0xFF198754);
-  static const Color textInputLabel = Color(0xFF0F3F62);
-  static const Color textInputBackgroundDefault = Color(0xFFF2F1F6);
-  static const Color textInputBackgroundFocused = Color(0xFFFFFFFF);
-  static const Color textInputBorderDefault = Color(0x00000000);
-  static const Color textInputBorderFocused = Color(0xFFB5B5C3);
-  static const Color textInputRightIcon = Color(0xFFA2A1A6);
-  static const Color textInputHint = Color(0xFF808080);
-  static const Color textInputText = Color(0xFF000000);
-  static const Color iconButtonIcon = Color(0xFFB5B5C3);
-  static const Color iconButtonBackground = Color(0xFFF2F1F6);
-  static const Color iconButtonBackIcon = Color(0xFF0F3F62);
-  static const Color appBarBackground = Color(0xFFFFFFFF);
-  static const Color alertDialogErrorTitle = Color(0xFFF64E60);
-  static const Color alertDialogInfoTitle = Color(0xFF0F3F62);
-  static const Color circleAvatarBackground = Color(0xFFF2F1F6);
-  static const Color white = Color(0xFFFFFFFF);
-  static const Color iconButtonBackgroundUnavailable = Color(0xFFE5E5E5);
-  static const Color containerBackgroundSettingsAppointment = Color(0xFFE5E5E5);
+  static const Color primary = Color(0xFF4682B4);
+  static const Color secondary = Color(0xFF23415A);
 }
 
 final lightTheme = ThemeData(
   brightness: Brightness.light,
-  primaryColor: const Color(0xFF4682B4),
-  secondaryHeaderColor: const Color(0xFF23415A),
+  primaryColor: MyColors.primary,
+  secondaryHeaderColor: MyColors.secondary,
   colorScheme: const ColorScheme.light(
     background: Color(0xFFE5E5E5),
+  ),
+  dialogTheme: const DialogTheme(
+    backgroundColor: Color(0xFFFFFFFF),
+    titleTextStyle: TextStyle(
+      color: MyColors.secondary,
+      fontWeight: FontWeight.bold,
+      fontSize: 20.0,
+    ),
   ),
   inputDecorationTheme: const InputDecorationTheme(
     filled: false,
@@ -69,10 +56,18 @@ final lightTheme = ThemeData(
 
 final darkTheme = ThemeData(
   brightness: Brightness.dark,
-  primaryColor: const Color(0xFF23415A),
-  secondaryHeaderColor: const Color(0xFF4682B4),
+  primaryColor: MyColors.secondary,
+  secondaryHeaderColor: MyColors.primary,
   colorScheme: const ColorScheme.dark(
     background: Color(0xFF1E1E1E),
+  ),
+  dialogTheme: const DialogTheme(
+    backgroundColor: Color(0xFF4E4E4E),
+    titleTextStyle: TextStyle(
+      color: MyColors.secondary,
+      fontWeight: FontWeight.bold,
+      fontSize: 20.0,
+    ),
   ),
   inputDecorationTheme: const InputDecorationTheme(
     filled: false,
