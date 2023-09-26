c7dea59d24ae37fe9c5433184d007e47a3e3e734
tbellicha <tanguy.bellicha@epitech.eu>
2023-09-26 06:35:06 +0200

    [ADD] Login Unit Testing (56.8%)
    Signed-off-by: tbellicha <tanguy.bellicha@epitech.eu>

--- Application/mobile/app_code/integration_test/login.dart
+++ Application/mobile/app_code/integration_test/login.dart
@@ -1,43 +1,92 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+import 'package:risu/components/text_input.dart';
 import 'package:risu/pages/login/login_page.dart';
+import 'package:risu/utils/theme.dart';
 
 void main() {
-  testWidgets('LoginPage Integration Test', (WidgetTester tester) async {
-    // Build the app and navigate to the login page
-    await tester.pumpWidget(
-      const MaterialApp(home: LoginPage()),
-    );
+  group('Test Login', () {
+    setUpAll(() async {
+      // This code runs once before all the tests.
+      WidgetsFlutterBinding.ensureInitialized();
+      WidgetController.hitTestWarningShouldBeFatal = true;
+    });
 
-    await tester.pumpAndSettle(const Duration(seconds: 2));
-    // Verify the initial state of the login page
-    expect(find.byKey(const Key('title-text')), findsOneWidget);
-    expect(find.byKey(const Key('email-text_input')), findsOneWidget);
-    expect(find.byKey(const Key('continue_email-button')), findsOneWidget);
-    expect(find.byKey(const Key('goto_signup-button')), findsOneWidget);
+    tearDown(() {
+      // This code runs after each test case.
+    });
 
-    // Tap the "Continuer avec un e-mail" button
-    await tester.tap(find.byKey(const Key('continue_email-button')));
-    await tester.pumpAndSettle();
+    testWidgets('Login', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MultiProvider(
+          providers: [
+            ChangeNotifierProvider<ThemeProvider>(
+              create: (_) =>
+                  ThemeProvider(false), // Provide a default value for testing.
+            ),
+          ],
+          child: const MaterialApp(
+            home: LoginPage(),
+          ),
+        ),
+      );
 
-    // Verify the state after tapping the "Continuer avec un e-mail" button
-    expect(find.byKey(const Key('title-text')), findsOneWidget);
-    expect(find.byKey(const Key('email-text_input')), findsOneWidget);
-    expect(find.byKey(const Key('login-button')), findsOneWidget);
-    expect(find.byKey(const Key('continue_email-button')), findsNothing);
-    expect(find.byKey(const Key('reset_password-button')), findsOneWidget);
+      Finder subTitleFinder = find.byKey(const Key('login-text_title'));
+      Finder textInputEmailFinder =
+          find.byKey(const Key('login-textinput_email'));
+      Finder textInputPasswordFinder =
+          find.byKey(const Key('login-textinput_password'));
+      Finder textButtonResetPasswordFinder =
+          find.byKey(const Key('login-textbutton_resetpassword'));
+      Finder buttonSigninFinder = find.byKey(const Key('login-button_signin'));
+      Finder textinputRightIconFinder =
+          find.byKey(const Key('textinput-button_righticon'));
+      Finder textButtonSignupFinder =
+          find.byKey(const Key('login-textbutton_gotosignup'));
+      Finder alertdialogEmptyFieldsFinder =
+          find.byKey(const Key('login-alertdialog_emptyfields'));
+      Finder alertdialogConnectionRefusedFinder =
+          find.byKey(const Key('login-alertdialog_connectionrefused'));
+      Finder alertdialogInvalidDataFinder =
+          find.byKey(const Key('login-alertdialog_invaliddata'));
+      Finder alertdialogInvalidTokenFinder =
+          find.byKey(const Key('login-alertdialog_invalidtoken'));
+      Finder alertdialogInvalidResponseFinder =
+          find.byKey(const Key('login-alertdialog_invalidresponse'));
+      Finder alertdialogInvalidCredentialsFinder =
+          find.byKey(const Key('login-alertdialog_invalidcredentials'));
+      Finder alertdialogErrorFinder =
+          find.byKey(const Key('login-alertdialog_error'));
 
-    // Enter the email and password
-    await tester.enterText(
-        find.byKey(const Key('email-text_input')), 'test@example.com');
-    await tester.enterText(
-        find.byKey(const Key('password-text_input')), 'password');
+      expect(subTitleFinder, findsOneWidget);
+      expect(textInputEmailFinder, findsOneWidget);
+      expect(textInputPasswordFinder, findsOneWidget);
+      expect(textinputRightIconFinder, findsOneWidget);
+      expect(textButtonResetPasswordFinder, findsOneWidget);
+      expect(buttonSigninFinder, findsOneWidget);
+      expect(textButtonSignupFinder, findsOneWidget);
+      expect(alertdialogEmptyFieldsFinder, findsNothing);
 
-    // Tap the "Se connecter" button
-    await tester.tap(find.byKey(const Key('login-button')));
-    await tester.pumpAndSettle();
+      await tester.tap(buttonSigninFinder);
+      await tester.pumpAndSettle();
 
-    // Verify the login succeeded and the user data is stored
-    expect(find.text('Connexion refused.'), findsOneWidget);
+      await tester.enterText(textInputEmailFinder, 'example@gmail.com');
+
+      expect(find.byKey(const Key('alertdialog-button_ok')), findsOneWidget);
+      await tester.tap(find.byKey(const Key('alertdialog-button_ok')));
+      await tester.pumpAndSettle();
+      expect(alertdialogEmptyFieldsFinder, findsNothing);
+
+      await tester.pumpAndSettle();
+      await tester.enterText(textInputPasswordFinder, '12345678');
+      await tester.pumpAndSettle();
+      expect(tester.widget<MyTextInput>(textInputPasswordFinder).obscureText,
+          true);
+      await tester.tap(textinputRightIconFinder);
+      await tester.pumpAndSettle();
+      expect(tester.widget<MyTextInput>(textInputPasswordFinder).obscureText,
+          false);
+    });
   });
 }
