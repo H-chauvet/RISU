3fdf36013c8c811f46e0d7808cb55a8877caada0
tbellicha <tanguy.bellicha@epitech.eu>
2023-09-26 03:48:40 +0200

    [ADD] First version of the map, waiting for the API
    Signed-off-by: tbellicha <tanguy.bellicha@epitech.eu>

--- Application/mobile/app_code/lib/pages/map/map_state.dart
+++ Application/mobile/app_code/lib/pages/map/map_state.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:risu/utils/theme.dart';
 
 import 'map_page.dart';
 
 class MapPageState extends State<MapPage> {
+  late GoogleMapController mapController;
+
   @override
   void initState() {
     super.initState();
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    setState(() {
+      mapController = controller;
+    });
   }
 
   @override
@@ -16,8 +25,12 @@ class MapPageState extends State<MapPage> {
       resizeToAvoidBottomInset: false,
       backgroundColor: context.select((ThemeProvider themeProvider) =>
           themeProvider.currentTheme.colorScheme.background),
-      body: const Center(
-        child: Text('Map Page'),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: const CameraPosition(
+          target: LatLng(0, 0), // Set the initial map location
+          zoom: 15.0, // Adjust the initial zoom level
+        ),
       ),
     );
   }
