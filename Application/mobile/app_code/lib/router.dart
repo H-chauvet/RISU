cdc9fffdcaa575927501df1ff413989df9962a48
Louismaestre <louis.maestre@epitech.eu>
2023-10-15 20:28:14 -0700

    add page in router

--- Application/mobile/app_code/lib/router.dart
+++ Application/mobile/app_code/lib/router.dart
@@ -18,7 +18,7 @@ final GoRouter router = GoRouter(
     GoRoute(
       path: '/',
       builder: (BuildContext context, GoRouterState state) {
-        return SettingsPage();
+        return PreAuthPage();
       },
       routes: <RouteBase>[
         GoRoute(
