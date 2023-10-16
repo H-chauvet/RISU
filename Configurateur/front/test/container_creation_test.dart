a7efe932adacca77306f8438984fd7152485e6ce
Cédric <cedric.corge@epitech.eu>
2023-10-16 19:07:20 +0200

    [FIX] test

--- Configurateur/front/test/container_creation_test.dart
+++ Configurateur/front/test/container_creation_test.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:front/screens/container-creation/container_creation.dart';
+import 'package:front/services/storage_service.dart';
 
 void main() {
   testWidgets('Container Creation progress bar', (WidgetTester tester) async {
     tester.binding.window.physicalSizeTestValue = const Size(5000, 5000);
     tester.binding.window.devicePixelRatioTestValue = 1.0;
+
+    token = "token";
+
     await tester.pumpWidget(const MaterialApp(
       home: ContainerCreation(),
     ));
@@ -17,6 +21,9 @@ void main() {
   testWidgets('Container Creation progress bar 2', (WidgetTester tester) async {
     tester.binding.window.physicalSizeTestValue = const Size(5000, 5000);
     tester.binding.window.devicePixelRatioTestValue = 1.0;
+
+    token = "token";
+
     await tester.pumpWidget(const MaterialApp(
       home: ContainerCreation(),
     ));
@@ -29,6 +36,9 @@ void main() {
   testWidgets('Container Creation progress bar 3', (WidgetTester tester) async {
     tester.binding.window.physicalSizeTestValue = const Size(5000, 5000);
     tester.binding.window.devicePixelRatioTestValue = 1.0;
+
+    token = "token";
+
     await tester.pumpWidget(const MaterialApp(
       home: ContainerCreation(),
     ));
@@ -42,6 +52,9 @@ void main() {
       (WidgetTester tester) async {
     tester.binding.window.physicalSizeTestValue = const Size(5000, 5000);
     tester.binding.window.devicePixelRatioTestValue = 1.0;
+
+    token = "token";
+
     await tester.pumpWidget(const MaterialApp(
       home: ContainerCreation(),
     ));
@@ -55,6 +68,9 @@ void main() {
       (WidgetTester tester) async {
     tester.binding.window.physicalSizeTestValue = const Size(5000, 5000);
     tester.binding.window.devicePixelRatioTestValue = 1.0;
+
+    token = "token";
+
     await tester.pumpWidget(const MaterialApp(
       home: ContainerCreation(),
     ));
@@ -71,6 +87,9 @@ void main() {
       (WidgetTester tester) async {
     tester.binding.window.physicalSizeTestValue = const Size(5000, 5000);
     tester.binding.window.devicePixelRatioTestValue = 1.0;
+
+    token = "token";
+
     await tester.pumpWidget(const MaterialApp(
       home: ContainerCreation(),
     ));
@@ -87,6 +106,9 @@ void main() {
       (WidgetTester tester) async {
     tester.binding.window.physicalSizeTestValue = const Size(5000, 5000);
     tester.binding.window.devicePixelRatioTestValue = 1.0;
+
+    token = "token";
+
     await tester.pumpWidget(const MaterialApp(
       home: ContainerCreation(),
     ));
