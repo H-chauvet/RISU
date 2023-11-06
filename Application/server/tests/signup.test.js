c0bdf066a0a91c176fe043e0eb54d958acbde28c
BuildTools <hugo.lopes-pinto@epitech.eu>
2023-11-06 12:04:17 +0100

    [ADD] new test signup

---
+++ Application/server/tests/signup.test.js
@@ -0,0 +1,20 @@
+const request = require('supertest');
+const async = require('async');
+
+describe('POST /api/signup', () => {
+it('should create a new user', function (done) {
+    async.series(
+      [
+        function (callback) {
+          request('http://localhost:8080')
+            .post('/api/signup')
+            .set('Content-Type', 'application/json')
+            .set('Accept', 'application/json')
+            .send({ email: 'test@email.com', password: 'password' })
+            .expect(201, callback)
+        },
+      ],
+      done
+    )
+  })
+});
