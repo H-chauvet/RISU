148a2223d9f10bbfbdaa0bb15b8e3fa465823717
tbellicha <tanguy.bellicha@epitech.eu>
2023-11-08 03:57:36 +0100

    [UPDATE] Remove logged in security
    Signed-off-by: tbellicha <tanguy.bellicha@epitech.eu>

--- Application/mobile/app_code/lib/pages/profile/profile_state.dart
+++ Application/mobile/app_code/lib/pages/profile/profile_state.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:risu/components/outlined_button.dart';
-import 'package:risu/network/informations.dart';
-import 'package:risu/pages/Settings/settings_page.dart';
-import 'package:risu/pages/login/login_page.dart';
+import 'package:risu/globals.dart';
 import 'package:risu/pages/pre_auth/pre_auth_page.dart';
 import 'package:risu/pages/profile/informations/informations_page.dart';
+import 'package:risu/pages/settings/settings_page.dart';
 import 'package:risu/utils/theme.dart';
 
 import 'profile_page.dart';
@@ -18,78 +17,74 @@ class ProfilePageState extends State<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
-    if (userInformation == null) {
-      return const LoginPage();
-    } else {
-      return Scaffold(
-        resizeToAvoidBottomInset: false,
-        backgroundColor: context.select((ThemeProvider themeProvider) =>
-            themeProvider.currentTheme.colorScheme.background),
-        body: SingleChildScrollView(
-          child: Center(
-            child: Container(
-              margin: const EdgeInsets.symmetric(horizontal: 32, vertical: 32),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(height: 16),
-                  SizedBox(
-                    width: double.infinity,
-                    child: MyOutlinedButton(
-                      text: 'Informations',
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) {
-                              return const ProfileInformationsPage();
-                            },
-                          ),
-                        );
-                      },
-                    ),
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: context.select((ThemeProvider themeProvider) =>
+          themeProvider.currentTheme.colorScheme.background),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 32, vertical: 32),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 16),
+                SizedBox(
+                  width: double.infinity,
+                  child: MyOutlinedButton(
+                    text: 'Informations',
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return const ProfileInformationsPage();
+                          },
+                        ),
+                      );
+                    },
                   ),
-                  const SizedBox(height: 16),
-                  SizedBox(
-                    width: double.infinity,
-                    child: MyOutlinedButton(
-                      text: 'Paramètres',
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) {
-                              return const SettingsPage();
-                            },
-                          ),
-                        );
-                      },
-                    ),
+                ),
+                const SizedBox(height: 16),
+                SizedBox(
+                  width: double.infinity,
+                  child: MyOutlinedButton(
+                    text: 'Paramètres',
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return const SettingsPage();
+                          },
+                        ),
+                      );
+                    },
                   ),
-                  const SizedBox(height: 16),
-                  SizedBox(
-                    width: double.infinity,
-                    child: MyOutlinedButton(
-                      text: 'Déconnexion',
-                      onPressed: () {
-                        userInformation = null;
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) {
-                              return const PreAuthPage();
-                            },
-                          ),
-                        );
-                      },
-                    ),
-                  )
-                ],
-              ),
+                ),
+                const SizedBox(height: 16),
+                SizedBox(
+                  width: double.infinity,
+                  child: MyOutlinedButton(
+                    text: 'Déconnexion',
+                    onPressed: () {
+                      userInformation = null;
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return const PreAuthPage();
+                          },
+                        ),
+                      );
+                    },
+                  ),
+                )
+              ],
             ),
           ),
         ),
-      );
-    }
+      ),
+    );
   }
 }
