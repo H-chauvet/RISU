8fbf7f2eb2eeb67e64c88b9694ea05f300d48680
Nathan <nathan.rousseau@epitech.eu>
2023-11-20 08:05:53 +0100

    [ADD] Basic Knowledge page

---
+++ Application/mobile/app_code/lib/pages/knowledge/knowledge_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'knowledge_state.dart';
+
+class KnowledgePage extends StatefulWidget {
+  const KnowledgePage({Key? key}) : super(key: key);
+
+  @override
+  State<KnowledgePage> createState() => KnowledgePageState();
+}
---
+++ Application/mobile/app_code/lib/pages/knowledge/knowledge_state.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+import 'knowledge_page.dart';
+
+class KnowledgePageState extends State<KnowledgePage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
