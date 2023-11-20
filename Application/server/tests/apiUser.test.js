71d30d1e5db3ffebd9e2401aa777650bba7d021a
tbellicha <tanguy.bellicha@epitech.eu>
2023-11-20 05:02:49 +0100

    [ADD] Tests on user deletion

--- Application/server/tests/apiUser.test.js
+++ Application/server/tests/apiUser.test.js
@@ -1,9 +1,19 @@
 const request = require('supertest');
 const async = require('async');
 
-let authToken = '';
-
 describe('POST /api/signup', () => {
+    let authToken;
+
+    beforeAll(async () => {
+      const loginResponse = await request('http://localhost:8080')
+        .post('/api/login')
+        .send({ email: 'user@gmail.com', password: 'user' });
+
+      userToken = loginResponse.body.data.token;
+      userID = loginResponse.body.data.user.id;
+      console.log('_________' + userToken)
+      console.log('_________' + userID)
+    });
     it('should connect and get a token', (done) => {
       async.series(
         [
@@ -60,6 +70,51 @@ describe('POST /api/signup', () => {
               .set('Accept', 'application/json')
               .set('Authorization', 'invalidToken')
               .expect(401, callback)
+          },
+        ],
+        done
+      )
+    }),
+    it('Should not delete the user, invalid ID', function (done) {
+      async.series(
+        [
+          function (callback) {
+            request('http://localhost:8080')
+              .delete('/api/user/invalidID')
+              .set('Content-Type', 'application/json')
+              .set('Accept', 'application/json')
+              .set('Authorization', 'invalidToken')
+              .expect(401, callback)
+          },
+        ],
+        done
+      )
+    }),
+    it('Should not delete the user, invalid Token', function (done) {
+      async.series(
+        [
+          function (callback) {
+            request('http://localhost:8080')
+              .delete(`/api/user/${userID}`)
+              .set('Content-Type', 'application/json')
+              .set('Accept', 'application/json')
+              .set('Authorization', `Bearer blablabla`)
+              .expect(401, callback)
+          },
+        ],
+        done
+      )
+    }),
+    it('Should delete the user', function (done) {
+      async.series(
+        [
+          function (callback) {
+            request('http://localhost:8080')
+              .delete(`/api/user/${userID}`)
+              .set('Content-Type', 'application/json')
+              .set('Accept', 'application/json')
+              .set('Authorization', `Bearer ${userToken}`)
+              .expect(200, callback)
           },
         ],
         done
