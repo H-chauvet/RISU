c69269c9017c072450c1c2f2fa606cbed1e73749
tbellicha <tanguy.bellicha@epitech.eu>
2023-11-22 01:36:09 +0100

    [FIX] ContactPage unit testing on Mobile
    Signed-off-by: tbellicha <tanguy.bellicha@epitech.eu>

--- Application/mobile/app_code/test/contact_test.dart
+++ Application/mobile/app_code/test/contact_test.dart
@@ -2,17 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:mockito/mockito.dart';
+import 'package:provider/provider.dart';
+import 'package:risu/globals.dart';
 import 'package:risu/pages/contact/contact_page.dart';
+import 'package:risu/utils/theme.dart';
+import 'package:risu/utils/user_data.dart';
 
 // Create a mock HTTP client for testing
 class MockHttpClient extends Mock implements http.Client {}
 
 void main() {
   testWidgets('ContactPage widget test', (WidgetTester tester) async {
-    // Build our widget and trigger a frame.
+    userInformation = UserData(
+        email: 'example@gmail.com', firstName: 'Example', lastName: 'Gmail');
     await tester.pumpWidget(
-      MaterialApp(
-        home: ContactPage(),
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider<ThemeProvider>(
+            create: (_) => ThemeProvider(false),
+          ),
+        ],
+        child: const MaterialApp(
+          home: ContactPage(),
+        ),
       ),
     );
 
@@ -30,6 +42,6 @@ void main() {
     // Simulate button tap
     final sendButton = find.byKey(Key('new-contact-button'));
     await tester.tap(sendButton);
-    await tester.pump();
+    await tester.pumpAndSettle();
   });
 }
