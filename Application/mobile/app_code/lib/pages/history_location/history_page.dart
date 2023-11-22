bb93ee1b39d3321d3841f09620c9df69b255941f
tbellicha <tanguy.bellicha@epitech.eu>
2023-11-22 03:03:49 +0100

    [REMOVE] Old files
    Signed-off-by: tbellicha <tanguy.bellicha@epitech.eu>

--- Application/mobile/app_code/lib/pages/history_location/history_page.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'history_state.dart';
-
-class HistoryLocationPage extends StatefulWidget {
-
-  @override
-  State<HistoryLocationPage> createState() => HistoryLocationState();
-}
