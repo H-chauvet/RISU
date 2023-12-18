eb08128a71546a545e822a95361fb63ad2ba67ed
Nathan <nathan.rousseau@epitech.eu>
2023-12-18 08:19:16 +0100

    [FIX] Small typo in configuration

--- Application/mobile/app_code/lib/globals.dart
+++ Application/mobile/app_code/lib/globals.dart
@@ -2,7 +2,7 @@ import 'package:risu/utils/user_data.dart';
 
 // Variable globale pour stocker l'adresse IP du serveur
 
-//String? serverIp = '10.0.2.2'; // A garder en local
+// String? serverIp = '10.0.2.2'; // A garder en local
 
 String? serverIp = '20.111.37.124'; // A mettre quand push github
 
