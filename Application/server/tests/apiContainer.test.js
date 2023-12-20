3b7fd1810ba29093a64a1e572a1225e2725a4c89
Quentin Leroueil <quentin.leroueil@epitech.eu>
2023-12-20 18:05:52 +0100

    add tests
    - check success case
    - check errors cases:
     - empty id
     - null id
    todo :
     - error case wrong id

---
+++ Application/server/tests/apiContainer.test.js
@@ -0,0 +1,50 @@
+const request = require('supertest');
+const async = require('async');
+
+describe('POST /containerDetails', function () {
+	it('should get container details from id', function (done) {
+		async.series(
+			[
+				function (callback) {
+					request('http://localhost:8080')
+						.post('/api/container/details')
+						.set('Content-Type', 'application/json')
+            .set('Accept', 'application/json')
+            .send({'containerId': '1234'})
+            .expect(200, callback)
+				}
+			],
+			done
+		)
+	}),
+	it('should not get container details from empty id', function (done) {
+		async.series(
+			[
+				function (callback) {
+					request('http://localhost:8080')
+						.post('/api/container/details')
+						.set('Content-Type', 'application/json')
+            .set('Accept', 'application/json')
+            .send({'containerId': ''})
+            .expect(401, callback)
+				}
+			],
+			done
+		)
+	}),
+	it('should not get container details from null id', function (done) {
+		async.series(
+			[
+				function (callback) {
+					request('http://localhost:8080')
+						.post('/api/container/details')
+						.set('Content-Type', 'application/json')
+            .set('Accept', 'application/json')
+            .send({'containerId': null})
+            .expect(401, callback)
+				}
+			],
+			done
+		)
+	})
+})
