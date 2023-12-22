3bb778238d9fc27722120c03b5519837b6a9c484
Louismaestre <louis.maestre@epitech.eu>
2023-12-21 16:47:23 -0800

    [ADD] back for the items

---
+++ Configurateur/back/controllers/object.js
@@ -0,0 +1,49 @@
+const { db } = require('../middleware/database')
+
+exports.getAllObject = containerId => {
+  return db.Object.findMany({
+    where: {
+        containerId: containerId
+    }
+  })
+}
+
+exports.getObject = id => {
+    return db.Object.findUnique({
+      where: {
+          id: id
+      }
+    })
+  }
+
+
+exports.createObject2 = object => {
+  object.price = 10;
+  return db.Object.create({
+    data: object
+  })
+}
+
+exports.deleteObject = id => {
+  return db.Object.delete({
+    where: {
+      id: id
+    }
+  })
+}
+
+exports.createObject = object => {
+  object.price = parseFloat(object.price)
+  object.height = parseFloat(object.height)
+  object.width = parseFloat(object.width)
+  return db.Object.create({
+    data: object
+  })
+}
+
+exports.createObject2 = object => {
+  object.price = parseFloat(object.price)
+  return db.Object.create({
+    data: object
+  })
+}
