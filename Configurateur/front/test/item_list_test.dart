7a4ff1e6d8743635015ca28a8e6f5ad698daff43
Louismaestre <louis.maestre@epitech.eu>
2023-12-23 13:39:23 -0800

    [ADD] web front test

---
+++ Configurateur/front/test/item_list_test.dart
@@ -0,0 +1,32 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:front/screens/container-list/item-list/item_component.dart';
+import 'package:front/screens/company/company.dart';
+
+void main() {
+  
+  testWidgets('UserMobileCard displays message details', (WidgetTester tester) async {
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: ItemCard(
+          item: ItemList(
+            id: 1,
+            name: "Hello world",
+            price: 10.0,
+            createdAt: null,
+            available: true,
+            containerId: 2,
+            container: 0,
+          ),
+          onDelete: (message) {},
+        ),
+      ),
+    );
+
+    expect(find.text(1.toString()), findsOneWidget);
+    expect(find.text("Hello world"), findsOneWidget);
+  });
+}
