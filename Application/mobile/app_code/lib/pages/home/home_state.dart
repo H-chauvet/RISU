f7acc9987a99f55023098be81ffef9cebcfe69aa
Nathan <nathan.rousseau@epitech.eu>
2024-01-10 18:36:00 +0100

    [ADD] First WIP of burger menu

--- Application/mobile/app_code/lib/pages/home/home_state.dart
+++ Application/mobile/app_code/lib/pages/home/home_state.dart
@@ -5,11 +5,16 @@ import 'package:risu/components/appbar.dart';
 import 'package:risu/components/bottomnavbar.dart';
 import 'package:risu/globals.dart';
 import 'package:risu/pages/article/list_page.dart';
+import 'package:risu/pages/login/login_page.dart';
 import 'package:risu/pages/map/map_page.dart';
 import 'package:risu/pages/profile/profile_page.dart';
+import 'package:risu/pages/settings/settings_page.dart';
 import 'package:risu/utils/check_signin.dart';
 import 'package:risu/utils/theme.dart';
 
+import '../../components/parameter.dart';
+import '../opinion/opinion_page.dart';
+import '../profile/informations/informations_page.dart';
 import 'home_page.dart';
 
 class HomePageState extends State<HomePage> {
@@ -76,6 +81,66 @@ class HomePageState extends State<HomePage> {
           showBackButton: false,
           showLogo: true,
           showBurgerMenu: false,
+        ),
+        endDrawer: SizedBox(
+          width: MediaQuery.of(context).size.width * 0.6, // 60 % of the screen
+          child: Drawer(
+            child: ListView(
+              // Important: Remove any padding from the ListView.
+              padding: EdgeInsets.zero,
+              children: [
+                SizedBox(
+                  height: 128,
+                  child: DrawerHeader(
+                    padding: const EdgeInsets.only(left: 32, top: 8),
+                    decoration: BoxDecoration(
+                      color: context.select((ThemeProvider themeProvider) =>
+                          themeProvider.currentTheme.secondaryHeaderColor),
+                    ),
+                    child: const Text(
+                      'Menu',
+                      style: TextStyle(
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 8, vertical: 0),
+                    child: Column(
+                      children: [
+                        SizedBox(height: 16),
+                        MyParameter(
+                          goToPage: _pages[2],
+                          title: 'Profil',
+                          paramIcon: Icon(Icons.person),
+                        ),
+                        SizedBox(height: 8),
+                        MyParameter(
+                          goToPage: ProfileInformationsPage(),
+                          title: 'Notifications',
+                          paramIcon: Icon(Icons.notifications_active),
+                          locked: true,
+                        ),
+                        SizedBox(height: 8),
+                        MyParameter(
+                          goToPage: OpinionPage(),
+                          title: 'Avis',
+                          paramIcon: Icon(Icons.star),
+                        ),
+                        SizedBox(height: 8),
+                        MyParameter(
+                          goToPage: SettingsPage(),
+                          title: 'Paramètres',
+                          paramIcon: Icon(Icons.settings),
+                        ),
+                      ],
+                    )),
+              ],
+            ),
+          ),
         ),
         body: _pages[_currentIndex],
         bottomNavigationBar: BottomNavBar(
