4c87bd12a7f52d17185764304e045a0785eb2874
hugolopespinto <72020673+hugolopespinto@users.noreply.github.com>
2024-01-10 12:21:16 +0100

    [UPDATE] fix warnings

--- Application/mobile/app_code/test/opinion_test.dart
+++ Application/mobile/app_code/test/opinion_test.dart
@@ -33,15 +33,15 @@ void main() {
       expect(addOpinionButtonFinder, findsOneWidget);
       expect(opinionButtonFilterFinder, findsOneWidget);
 
-      await tester.tap(opinionButtonFilterFinder);
+      await tester.tap(opinionButtonFilterFinder, warnIfMissed: false);
       await tester.pump();
 
       Finder filterAll = find.byKey(const Key('opinion-filter_dropdown_all'));
 
-      await tester.tap(filterAll);
+      await tester.tap(filterAll, warnIfMissed: false);
       await tester.pump();
 
-      await tester.tap(addOpinionButtonFinder);
+      await tester.tap(addOpinionButtonFinder, warnIfMissed: false);
       await tester.pump();
 
       expect(find.text('Ajouter un avis'), findsOneWidget);
@@ -51,7 +51,7 @@ void main() {
       expect(find.byKey(const Key('opinion-star_3')), findsOneWidget);
       expect(find.byKey(const Key('opinion-star_4')), findsOneWidget);
 
-      await tester.tap(find.byKey(const Key('opinion-star_4')));
+      await tester.tap(find.byKey(const Key('opinion-star_4')), warnIfMissed: false);
       await tester.pump();
     });
     testWidgets('select filter 0 stars', (WidgetTester tester) async {
@@ -80,15 +80,15 @@ void main() {
       expect(addOpinionButtonFinder, findsOneWidget);
       expect(opinionButtonFilterFinder, findsOneWidget);
 
-      await tester.tap(opinionButtonFilterFinder);
+      await tester.tap(opinionButtonFilterFinder, warnIfMissed: false);
       await tester.pump();
 
       Finder filter = find.byKey(const Key('opinion-filter_dropdown'));
-      await tester.tap(filter);
+      await tester.tap(filter, warnIfMissed: false);
       await tester.pump();
 
       Finder filterAll = find.byKey(const Key('opinion-filter_dropdown_all'));
-      await tester.tap(filterAll);
+      await tester.tap(filterAll, warnIfMissed: false);
       await tester.pump();
 
       expect(find.text('0 étoiles'), findsOneWidget);
@@ -100,7 +100,7 @@ void main() {
       expect(find.text('Tous les avis'), findsNWidgets(2));
 
       Finder filter_0 = find.byKey(const Key('opinion-filter_dropdown_0'));
-      await tester.tap(filter_0);
+      await tester.tap(filter_0, warnIfMissed: false);
       await tester.pump();
     });
     testWidgets('select filter 1 stars', (WidgetTester tester) async {
@@ -129,19 +129,19 @@ void main() {
       expect(addOpinionButtonFinder, findsOneWidget);
       expect(opinionButtonFilterFinder, findsOneWidget);
 
-      await tester.tap(opinionButtonFilterFinder);
+      await tester.tap(opinionButtonFilterFinder, warnIfMissed: false);
       await tester.pump();
 
       Finder filter = find.byKey(const Key('opinion-filter_dropdown'));
-      await tester.tap(filter);
+      await tester.tap(filter, warnIfMissed: false);
       await tester.pump();
 
       Finder filterAll = find.byKey(const Key('opinion-filter_dropdown_all'));
-      await tester.tap(filterAll);
+      await tester.tap(filterAll, warnIfMissed: false);
       await tester.pump();
 
       Finder filter_1 = find.byKey(const Key('opinion-filter_dropdown_1'));
-      await tester.tap(filter_1);
+      await tester.tap(filter_1, warnIfMissed: false);
       await tester.pump();
     });
     testWidgets('select filter 2 stars', (WidgetTester tester) async {
@@ -170,19 +170,19 @@ void main() {
       expect(addOpinionButtonFinder, findsOneWidget);
       expect(opinionButtonFilterFinder, findsOneWidget);
 
-      await tester.tap(opinionButtonFilterFinder);
+      await tester.tap(opinionButtonFilterFinder, warnIfMissed: false);
       await tester.pump();
 
       Finder filter = find.byKey(const Key('opinion-filter_dropdown'));
-      await tester.tap(filter);
+      await tester.tap(filter, warnIfMissed: false);
       await tester.pump();
 
       Finder filterAll = find.byKey(const Key('opinion-filter_dropdown_all'));
-      await tester.tap(filterAll);
+      await tester.tap(filterAll, warnIfMissed: false);
       await tester.pump();
 
       Finder filter_2 = find.byKey(const Key('opinion-filter_dropdown_2'));
-      await tester.tap(filter_2);
+      await tester.tap(filter_2, warnIfMissed: false);
       await tester.pump();
     });
     testWidgets('select filter 3 stars', (WidgetTester tester) async {
@@ -211,19 +211,19 @@ void main() {
       expect(addOpinionButtonFinder, findsOneWidget);
       expect(opinionButtonFilterFinder, findsOneWidget);
 
-      await tester.tap(opinionButtonFilterFinder);
+      await tester.tap(opinionButtonFilterFinder, warnIfMissed: false);
       await tester.pump();
 
       Finder filter = find.byKey(const Key('opinion-filter_dropdown'));
-      await tester.tap(filter);
+      await tester.tap(filter, warnIfMissed: false);
       await tester.pump();
 
       Finder filterAll = find.byKey(const Key('opinion-filter_dropdown_all'));
-      await tester.tap(filterAll);
+      await tester.tap(filterAll, warnIfMissed: false);
       await tester.pump();
 
       Finder filter_3 = find.byKey(const Key('opinion-filter_dropdown_3'));
-      await tester.tap(filter_3);
+      await tester.tap(filter_3, warnIfMissed: false);
       await tester.pump();
     });
     testWidgets('select filter 4 stars', (WidgetTester tester) async {
@@ -252,19 +252,19 @@ void main() {
       expect(addOpinionButtonFinder, findsOneWidget);
       expect(opinionButtonFilterFinder, findsOneWidget);
 
-      await tester.tap(opinionButtonFilterFinder);
+      await tester.tap(opinionButtonFilterFinder, warnIfMissed: false);
       await tester.pump();
 
       Finder filter = find.byKey(const Key('opinion-filter_dropdown'));
-      await tester.tap(filter);
+      await tester.tap(filter, warnIfMissed: false);
       await tester.pump();
 
       Finder filterAll = find.byKey(const Key('opinion-filter_dropdown_all'));
-      await tester.tap(filterAll);
+      await tester.tap(filterAll, warnIfMissed: false);
       await tester.pump();
 
       Finder filter_4 = find.byKey(const Key('opinion-filter_dropdown_4'));
-      await tester.tap(filter_4);
+      await tester.tap(filter_4, warnIfMissed: false);
       await tester.pump();
     });
     testWidgets('select filter 5 stars', (WidgetTester tester) async {
@@ -293,19 +293,19 @@ void main() {
       expect(addOpinionButtonFinder, findsOneWidget);
       expect(opinionButtonFilterFinder, findsOneWidget);
 
-      await tester.tap(opinionButtonFilterFinder);
+      await tester.tap(opinionButtonFilterFinder, warnIfMissed: false);
       await tester.pump();
 
       Finder filter = find.byKey(const Key('opinion-filter_dropdown'));
-      await tester.tap(filter);
+      await tester.tap(filter, warnIfMissed: false);
       await tester.pump();
 
       Finder filterAll = find.byKey(const Key('opinion-filter_dropdown_all'));
-      await tester.tap(filterAll);
+      await tester.tap(filterAll, warnIfMissed: false);
       await tester.pump();
 
       Finder filter_5 = find.byKey(const Key('opinion-filter_dropdown_5'));
-      await tester.tap(filter_5);
+      await tester.tap(filter_5, warnIfMissed: false);
       await tester.pump();
     });
     testWidgets('no opinion', (WidgetTester tester) async {
@@ -334,19 +334,19 @@ void main() {
       expect(addOpinionButtonFinder, findsOneWidget);
       expect(opinionButtonFilterFinder, findsOneWidget);
 
-      await tester.tap(opinionButtonFilterFinder);
+      await tester.tap(opinionButtonFilterFinder, warnIfMissed: false);
       await tester.pump();
 
       Finder filter = find.byKey(const Key('opinion-filter_dropdown'));
-      await tester.tap(filter);
+      await tester.tap(filter, warnIfMissed: false);
       await tester.pump();
 
       Finder filterAll = find.byKey(const Key('opinion-filter_dropdown_all'));
-      await tester.tap(filterAll);
+      await tester.tap(filterAll, warnIfMissed: false);
       await tester.pump();
 
       Finder filter_3 = find.byKey(const Key('opinion-filter_dropdown_3'));
-      await tester.tap(filter_3);
+      await tester.tap(filter_3, warnIfMissed: false);
       await tester.pump();
 
       expect(find.text('Aucun avis'), findsOneWidget);
@@ -377,15 +377,15 @@ void main() {
       expect(addOpinionButtonFinder, findsOneWidget);
       expect(opinionButtonFilterFinder, findsOneWidget);
 
-      await tester.tap(opinionButtonFilterFinder);
+      await tester.tap(opinionButtonFilterFinder, warnIfMissed: false);
       await tester.pump();
 
       Finder filterAll = find.byKey(const Key('opinion-filter_dropdown_all'));
 
-      await tester.tap(filterAll);
+      await tester.tap(filterAll, warnIfMissed: false);
       await tester.pump();
 
-      await tester.tap(addOpinionButtonFinder);
+      await tester.tap(addOpinionButtonFinder, warnIfMissed: false);
       await tester.pump();
 
       expect(find.text('Ajouter un avis'), findsOneWidget);
@@ -395,15 +395,15 @@ void main() {
       expect(find.byKey(const Key('opinion-star_3')), findsOneWidget);
       expect(find.byKey(const Key('opinion-star_4')), findsOneWidget);
 
-      await tester.tap(find.byKey(const Key('opinion-star_0')));
+      await tester.tap(find.byKey(const Key('opinion-star_0')), warnIfMissed: false);
       await tester.pump();
-      await tester.tap(find.byKey(const Key('opinion-star_1')));
+      await tester.tap(find.byKey(const Key('opinion-star_1')), warnIfMissed: false);
       await tester.pump();
-      await tester.tap(find.byKey(const Key('opinion-star_2')));
+      await tester.tap(find.byKey(const Key('opinion-star_2')), warnIfMissed: false);
       await tester.pump();
-      await tester.tap(find.byKey(const Key('opinion-star_3')));
+      await tester.tap(find.byKey(const Key('opinion-star_3')), warnIfMissed: false);
       await tester.pump();
-      await tester.tap(find.byKey(const Key('opinion-star_4')));
+      await tester.tap(find.byKey(const Key('opinion-star_4')), warnIfMissed: false);
       await tester.pump();
 
       // écrire 'test' dans le champ  qui a pour key 'opinion-textinput_comment'
@@ -411,7 +411,7 @@ void main() {
 
       final buttonFinder = find.byKey(const Key('opinion-button_add'));
       expect(buttonFinder, findsOneWidget);
-      await tester.tap(buttonFinder);
+      await tester.tap(buttonFinder, warnIfMissed: false);
       await tester.pump();
     });
     testWidgets('cancel button new opinion', (WidgetTester tester) async {
@@ -440,20 +440,20 @@ void main() {
       expect(addOpinionButtonFinder, findsOneWidget);
       expect(opinionButtonFilterFinder, findsOneWidget);
 
-      await tester.tap(opinionButtonFilterFinder);
+      await tester.tap(opinionButtonFilterFinder, warnIfMissed: false);
       await tester.pump();
 
       Finder filterAll = find.byKey(const Key('opinion-filter_dropdown_all'));
 
-      await tester.tap(filterAll);
+      await tester.tap(filterAll, warnIfMissed: false);
       await tester.pump();
 
-      await tester.tap(addOpinionButtonFinder);
+      await tester.tap(addOpinionButtonFinder, warnIfMissed: false);
       await tester.pump();
 
       final buttonFinder = find.byKey(const Key('cancel-button'));
       expect(buttonFinder, findsOneWidget);
-      await tester.tap(buttonFinder);
+      await tester.tap(buttonFinder, warnIfMissed: false);
       await tester.pump();
     });
   });
