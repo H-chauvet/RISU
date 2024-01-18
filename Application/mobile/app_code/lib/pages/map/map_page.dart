b35ebcd599b31b9c642484ed6c07f5199553b955
tbellicha <tanguy.bellicha@epitech.eu>
2024-01-18 21:28:36 +0100

    [FIX] Usage of the super for the key
    Signed-off-by: tbellicha <tanguy.bellicha@epitech.eu>

--- Application/mobile/app_code/lib/pages/map/map_page.dart
+++ Application/mobile/app_code/lib/pages/map/map_page.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'map_state.dart';
 
 class MapPage extends StatefulWidget {
-  const MapPage({super.key});
+  const MapPage({
+    super.key,
+  });
 
   @override
   State<MapPage> createState() => MapPageState();
--- Application/mobile/app_code/lib/pages/settings/settings_pages/theme/theme_settings_page.dart
+++ Application/mobile/app_code/lib/pages/settings/settings_pages/theme/theme_settings_page.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'theme_settings_state.dart';
 
 class ThemeSettingsPage extends StatefulWidget {
-  const ThemeSettingsPage({super.key});
+  const ThemeSettingsPage({
+    super.key,
+  });
 
   @override
   State<ThemeSettingsPage> createState() => ThemeSettingsPageState();
