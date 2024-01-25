e7d000015eb43344da8b7aa972ea32a746b8aa40
Louismaestre <louis.maestre@epitech.eu>
2024-01-25 10:21:31 -0800

    [UPDATE] change display string by 'address, city'

--- Application/mobile/app_code/lib/pages/container/details_state.dart
+++ Application/mobile/app_code/lib/pages/container/details_state.dart
@@ -115,7 +115,7 @@ class ContainerDetailsState extends State<ContainerDetailsPage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
-                  '$_city, $_address',
+                  '$_address, $_city',
                   key: const Key('container-details_title'),
                   style: TextStyle(
                     fontSize: 32,
