5220342b9245744c9bc751cd605b3a0fe6cc07ff
Cédric <cedric.corge@epitech.eu>
2024-01-26 16:08:34 +0100

    [REMOVE] useless setup file

--- Configurateur/back/jest.config.js
+++ Configurateur/back/jest.config.js
@@ -1,15 +1,13 @@
 module.exports = {
-  testEnvironment: "node", // ou 'jsdom' si vous travaillez avec le navigateur
-  roots: ["<rootDir>/"], // Chemin vers le répertoire principal de votre code source
-  testMatch: ["**/__tests__/**/*.js", "**/?(*.)+(spec|test).js"], // Modèle pour les fichiers de test
-  collectCoverage: true, // Active la collecte de la couverture de code
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "node",
+  roots: ["<rootDir>/"],
+  testMatch: ["**/__tests__/**/*.js", "**/?(*.)+(spec|test).js"],
+  collectCoverage: true,
   collectCoverageFrom: [
     "routes/**/*.js",
     "middleware/**/*.js",
     "controllers/**/*.js",
-  ], // Spécifie les fichiers à inclure dans la couverture
-  coverageDirectory: "<rootDir>/coverage", // Spécifie le répertoire de sortie pour les rapports de couverture
-  coverageReporters: ["text", "lcov"], // Rapports de couverture à générer
-  // ... d'autres configurations personnalisées peuvent être ajoutées ici
+  ],
+  coverageDirectory: "<rootDir>/coverage",
+  coverageReporters: ["text", "lcov"],
 };
--- Configurateur/back/jest.setup.js
+++
@@ -1,8 +0,0 @@
-// Exemple de jest.setup.js
-const { PrismaClient } = require("@prisma/client");
-const db = new PrismaClient();
-
-// Fermez le client Prisma après les tests
-afterAll(async () => {
-  await db.$disconnect();
-});
