7ad9bd6aabe68ef892ff2993eba0b6dd06ca7aa2
tbellicha <tanguy.bellicha@epitech.eu>
2024-02-01 08:41:14 +0100

    [UPDATE] Local serverIp

--- Application/mobile/app_code/lib/globals.dart
+++ Application/mobile/app_code/lib/globals.dart
@@ -1,10 +1,13 @@
+import 'package:http/io_client.dart';
 import 'package:risu/utils/user_data.dart';
 
 // Variable globale pour stocker l'adresse IP du serveur
 
-// String? serverIp = '10.0.2.2'; // A garder en local
+// String? serverIp = 'https://risu-epitech.com'; // A garder en local
 
 String? serverIp = '20.111.37.124'; // A mettre quand push github
 
 /// Global variable referencing the user network data
 UserData? userInformation;
+
+IOClient ioClient = IOClient();
