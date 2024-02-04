e63fa21298be718c6e3dd5ec496ec0150e0018a3
Henri Chauvet <henri.chauvet@epitech.eu>
2024-02-04 18:41:09 +0100

    [UPDATE] globals.dart

--- Application/mobile/app_code/lib/globals.dart
+++ Application/mobile/app_code/lib/globals.dart
@@ -4,7 +4,7 @@ import 'package:risu/utils/user_data.dart';
 
 // String? serverIp = '10.0.2.2'; // A garder en local
 
-String? serverIp = '20.111.37.124'; // A mettre quand push github
+String? serverIp = '51.103.94.191'; // A mettre quand push github
 
 /// Global variable referencing the user network data
 UserData? userInformation;
