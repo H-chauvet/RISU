1c08aedc04ec0ee383f05f9d4346531d3e7a540e
tbellicha <tanguy.bellicha@epitech.eu>
2024-02-05 09:11:23 +0100

    [ADD] Modal of languages choice
    Signed-off-by: tbellicha <tanguy.bellicha@epitech.eu>

---
+++ Application/mobile/app_code/lib/pages/settings/settings_pages/language/modal.dart
@@ -0,0 +1,82 @@
+import 'package:country_flags/country_flags.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:risu/components/toast.dart';
+import 'package:risu/globals.dart';
+import 'package:risu/utils/providers/language.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LanguageChangeModalContent extends StatelessWidget {
+  const LanguageChangeModalContent({super.key});
+
+  Future<String> getCurrentLanguage() async {
+    final prefs = await SharedPreferences.getInstance();
+    final List<String> items = ['fr', 'en'];
+    final currentLanguage = prefs.getString('language') ?? defaultLanguage;
+    return items[items.indexOf(currentLanguage)];
+  }
+
+  void changeLanguage(BuildContext context, String languageCode) {
+    Provider.of<LanguageProvider>(context, listen: false)
+        .changeLanguage(Locale(languageCode));
+  }
+
+  Widget displayLanguage(
+      BuildContext context, String languageCode, String languageName) {
+    return RadioListTile<String>(
+      key: Key('language_modal-radio_button_$languageCode'),
+      title: Row(
+        children: [
+          CountryFlag.fromLanguageCode(
+            languageCode,
+            width: 32,
+            height: 32,
+          ),
+          const SizedBox(width: 8),
+          Text(languageName),
+        ],
+      ),
+      value: languageCode,
+      groupValue:
+          Provider.of<LanguageProvider>(context).currentLocale.languageCode,
+      onChanged: (value) async {
+        if (value != null &&
+            value !=
+                Provider.of<LanguageProvider>(context, listen: false)
+                    .currentLocale
+                    .languageCode) {
+          changeLanguage(context, value);
+          MyToastMessage.show(
+            context: context,
+            message: "Language changed to $languageName",
+          );
+        }
+        Navigator.of(context).pop();
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        FutureBuilder<String>(
+          future: getCurrentLanguage(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return Column(
+                children: [
+                  displayLanguage(context, 'fr', "Français"),
+                  displayLanguage(context, 'en', "English"),
+                ],
+              );
+            } else {
+              return Container();
+            }
+          },
+        ),
+      ],
+    );
+  }
+}
