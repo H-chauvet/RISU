8baa64016fc52d32e2e12e37ccf1ae593c3517ff
tbellicha <tanguy.bellicha@epitech.eu>
2024-02-05 03:48:38 +0100

    [FIX] Format of the file
    Signed-off-by: tbellicha <tanguy.bellicha@epitech.eu>

--- Application/mobile/app_code/test/parameter_button_test.dart
+++ Application/mobile/app_code/test/parameter_button_test.dart
@@ -3,8 +3,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:risu/components/parameter.dart';
 import 'package:risu/pages/login/login_page.dart';
 import 'package:risu/pages/settings/settings_pages/theme/theme_settings_page.dart';
-import 'package:provider/provider.dart';
-import 'package:risu/utils/theme.dart';
 
 import 'globals.dart';
 
@@ -118,54 +116,3 @@ void main() {
     await tester.pumpAndSettle();
   });
 }
-
-// theme_settings_page.dart
-/*import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:provider/provider.dart';
-import 'package:risu/pages/settings/settings_pages/theme/theme_settings_page.dart';
-import 'package:risu/utils/theme.dart';
-
-import 'globals.dart';
-
-void main() {
-  group('Test Theme Settings', () {
-    setUpAll(() async {
-      // This code runs once before all the tests.
-      WidgetsFlutterBinding.ensureInitialized();
-      WidgetController.hitTestWarningShouldBeFatal = true;
-    });
-
-    tearDown(() {
-      // This code runs after each test case.
-    });
-
-    testWidgets('Light mode', (WidgetTester tester) async {
-      await tester.pumpWidget(initPage(const ThemeSettingsPage()));
-
-      final dropdownFinder = find.byKey(const Key('drop_down'));
-
-      final themeProvider = Provider.of<ThemeProvider>(
-          tester.element(find.byType(ThemeSettingsPage)),
-          listen: false);
-
-      expect(themeProvider.currentTheme.brightness, Brightness.light);
-      expect(dropdownFinder, findsOneWidget);
-      await tester.pumpAndSettle();
-    });
-    testWidgets('Dark mode', (WidgetTester tester) async {
-      await tester
-          .pumpWidget(initPage(const ThemeSettingsPage(), isDarkMode: true));
-
-      final dropdownFinder = find.byKey(const Key('drop_down'));
-
-      final themeProvider = Provider.of<ThemeProvider>(
-          tester.element(find.byType(ThemeSettingsPage)),
-          listen: false);
-
-      expect(themeProvider.currentTheme.brightness, Brightness.dark);
-      expect(dropdownFinder, findsOneWidget);
-      await tester.pumpAndSettle();
-    });
-  });
-}*/
