8d1b1cf84bbad1a0021f68859470a29cec06e050
tbellicha <tanguy.bellicha@epitech.eu>
2024-02-05 07:42:09 +0100

    [ADD] Tests on BurgerDrawer when logged in or not

--- Application/mobile/app_code/lib/components/burger_drawer.dart
+++ Application/mobile/app_code/lib/components/burger_drawer.dart
@@ -17,62 +17,67 @@ class BurgerDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-        width: MediaQuery.of(context).size.width * 0.6, // 60 % of the screen
-        child: Drawer(
-            child: Column(children: [
-          SizedBox(
-            height: 128,
-            width: double.infinity,
-            child: DrawerHeader(
-              padding: EdgeInsets.zero,
-              decoration: BoxDecoration(
-                color: context.select((ThemeProvider themeProvider) =>
-                    themeProvider.currentTheme.secondaryHeaderColor),
-              ),
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: Image.asset(
-                  key: const Key('appbar-image_logo'),
-                  'assets/logo_noir.png',
-                  height: 64,
+      width: MediaQuery.of(context).size.width * 0.6, // 60 % of the screen
+      child: Drawer(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 128,
+              width: double.infinity,
+              child: DrawerHeader(
+                padding: EdgeInsets.zero,
+                decoration: BoxDecoration(
+                  color: context.select((ThemeProvider themeProvider) =>
+                      themeProvider.currentTheme.secondaryHeaderColor),
+                ),
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  child: Image.asset(
+                    key: const Key('appbar-image_logo'),
+                    'assets/logo_noir.png',
+                    height: 64,
+                  ),
                 ),
               ),
             ),
-          ),
-          const SizedBox(height: 16),
-          const MyRedirectDivider(
-            goToPage: ProfileInformationsPage(),
-            title: 'Détails du profile',
-            paramIcon: Icon(Icons.person),
-          ),
-          const SizedBox(height: 8),
-          const MyRedirectDivider(
-            goToPage: NotificationsPage(),
-            title: 'Notifications',
-            paramIcon: Icon(Icons.notifications),
-          ),
-          const SizedBox(height: 8),
-          const MyRedirectDivider(
-            goToPage: OpinionPage(),
-            title: 'Avis',
-            paramIcon: Icon(Icons.star),
-          ),
-          const SizedBox(height: 8),
-          const MyRedirectDivider(
-            goToPage: SettingsPage(),
-            title: 'Paramètres',
-            paramIcon: Icon(Icons.settings),
-          ),
-          const Spacer(),
-          if (userInformation != null)
+            const SizedBox(height: 16),
             const MyRedirectDivider(
-              goToPage: LoginPage(),
-              title: 'Déconnexion',
-              paramIcon: Icon(Icons.logout),
-              disconnect: true,
-              chosenPlace: DIVIDERPLACE.top,
+              goToPage: ProfileInformationsPage(),
+              title: 'Détails du profile',
+              paramIcon: Icon(Icons.person),
             ),
-          const SizedBox(height: 8),
-        ])));
+            const SizedBox(height: 8),
+            const MyRedirectDivider(
+              goToPage: NotificationsPage(),
+              title: 'Notifications',
+              paramIcon: Icon(Icons.notifications),
+            ),
+            const SizedBox(height: 8),
+            const MyRedirectDivider(
+              goToPage: OpinionPage(),
+              title: 'Avis',
+              paramIcon: Icon(Icons.star),
+            ),
+            const SizedBox(height: 8),
+            const MyRedirectDivider(
+              goToPage: SettingsPage(),
+              title: 'Paramètres',
+              paramIcon: Icon(Icons.settings),
+            ),
+            const Spacer(),
+            if (userInformation != null)
+              const MyRedirectDivider(
+                key: Key('burgerdrawer-logout'),
+                goToPage: LoginPage(),
+                title: 'Déconnexion',
+                paramIcon: Icon(Icons.logout),
+                disconnect: true,
+                chosenPlace: DIVIDERPLACE.top,
+              ),
+            const SizedBox(height: 8),
+          ],
+        ),
+      ),
+    );
   }
 }
