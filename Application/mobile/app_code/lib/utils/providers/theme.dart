7fdc98a4c39c6b8c132f34dfb15e86b4f57bc7ae
tbellicha <tanguy.bellicha@epitech.eu>
2024-02-05 09:15:01 +0100

    [FIX] Use of relative path
    Signed-off-by: tbellicha <tanguy.bellicha@epitech.eu>

--- Application/mobile/app_code/lib/utils/providers/theme.dart
+++ Application/mobile/app_code/lib/utils/providers/theme.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:risu/utils/colors.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
-import '../colors.dart';
 
 class ThemeProvider extends ChangeNotifier {
   late ThemeData _currentTheme;
