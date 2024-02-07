f5ce9f403c3f20ac7177045540736e0b40afb613
tbellicha <tanguy.bellicha@epitech.eu>
2024-02-07 19:12:09 +0100

    [FIX] Unit tests on Validators
    Signed-off-by: tbellicha <tanguy.bellicha@epitech.eu>

--- Application/mobile/app_code/test/validators_test.dart
+++ Application/mobile/app_code/test/validators_test.dart
@@ -1,69 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:risu/utils/validators.dart';
 
-import 'globals.dart';
+// MockBuildContext class
+class MockBuildContext extends Mock implements BuildContext {
+  @override
+  T? dependOnInheritedWidgetOfExactType<T extends InheritedWidget>(
+      {Object? aspect}) {
+    if (T == AppLocalizations) {
+      return MockAppLocalizations() as T;
+    }
+    return null;
+  }
+}
+
+// MockAppLocalizations class
+class MockAppLocalizations extends Mock implements AppLocalizations {
+  @override
+  String get fieldRequired =>
+      AppLocalizations.of(MockBuildContext())!.fieldRequired;
+
+  @override
+  String get emailInvalid =>
+      AppLocalizations.of(MockBuildContext())!.emailInvalid;
+}
 
 void main() {
-  group('Validators Integration Test', () {
-    setUpAll(() async {
-      // This code runs once before all the tests.
-      WidgetsFlutterBinding.ensureInitialized();
+  group('Validators Test', () {
+    setUpAll(() {
+      TestWidgetsFlutterBinding.ensureInitialized();
       WidgetController.hitTestWarningShouldBeFatal = true;
     });
 
-    tearDown(() {
-      // This code runs after each test case.
-    });
-
-    testWidgets('Valid Email', (WidgetTester tester) async {
+    testWidgets('User info complete', (WidgetTester tester) async {
       await tester.pumpWidget(
-        initPage(
-          Scaffold(
-            body: Builder(
-              builder: (BuildContext context) {
-                return Text(
-                    Validators().email(context, 'example@email.com') ?? '');
-              },
-            ),
-          ),
+        MaterialApp(
+          home: Builder(builder: (BuildContext context) {
+            return Container();
+          }),
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
         ),
       );
+      BuildContext context = tester.element(find.byType(Container));
 
-      expect(find.text('Please enter a valid email.'), findsNothing);
-    });
+      expect(Validators().email(context, null),
+          AppLocalizations.of(context)!.fieldRequired);
+      expect(Validators().email(context, ''),
+          AppLocalizations.of(context)!.fieldRequired);
 
-    testWidgets('Valid Not Empty', (WidgetTester tester) async {
-      await tester.pumpWidget(
-        initPage(
-          Scaffold(
-            body: Builder(
-              builder: (BuildContext context) {
-                return Text(
-                    Validators().notEmpty(context, 'Valid Input') ?? '');
-              },
-            ),
-          ),
-        ),
-      );
+      expect(Validators().email(context, 'invalid_email'),
+          AppLocalizations.of(context)!.emailInvalid);
 
-      expect(find.text('This field is required'), findsNothing);
-    });
+      expect(Validators().email(context, 'test@example.com'), null);
 
-    testWidgets('Invalid Not Empty', (WidgetTester tester) async {
-      await tester.pumpWidget(
-        initPage(
-          Scaffold(
-            body: Builder(
-              builder: (BuildContext context) {
-                return Text(Validators().notEmpty(context, '') ?? '');
-              },
-            ),
-          ),
-        ),
-      );
+      expect(Validators().notEmpty(context, null),
+          AppLocalizations.of(context)!.fieldRequired);
+      expect(Validators().notEmpty(context, ''),
+          AppLocalizations.of(context)!.fieldRequired);
 
-      expect(find.text('This field is required'), findsOneWidget);
+      expect(Validators().notEmpty(context, 'not empty'), null);
     });
   });
 }
