537a2e0c55818748bd66a931a9fb691d2b89a807
BuildTools <hugo.lopes-pinto@epitech.eu>
2024-02-09 13:37:33 +0100

    [FIX] fix import

--- Application/mobile/app_code/lib/utils/providers/theme.dart
+++ Application/mobile/app_code/lib/utils/providers/theme.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:risu/utils/colors.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'colors.dart';
-
 dynamic appTheme = {
   'clair': 'Clair',
   'sombre': 'Sombre',
