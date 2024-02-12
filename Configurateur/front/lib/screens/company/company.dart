a02a7345e50f409cddc54f0e3f18684ecdf0c5a8
Cédric <cedric.corge@epitech.eu>
2024-02-12 16:30:26 +0100

    [ADD] good format for dart file

--- Configurateur/front/lib/screens/company/company.dart
+++ Configurateur/front/lib/screens/company/company.dart
@@ -35,13 +35,15 @@ class CompanyPageState extends State<CompanyPage> {
   }
 
   Future<void> fetchContainers() async {
-    final response =
-        await http.get(Uri.parse('http://${serverIp}:3000/api/container/listAll'));
+    final response = await http
+        .get(Uri.parse('http://${serverIp}:3000/api/container/listAll'));
     if (response.statusCode == 200) {
       final Map<String, dynamic> responseData = json.decode(response.body);
       final List<dynamic> containersData = responseData["container"];
       setState(() {
-        containers = containersData.map((data) => MyContainerList.fromJson(data)).toList();
+        containers = containersData
+            .map((data) => MyContainerList.fromJson(data))
+            .toList();
       });
     } else {
       Fluttertoast.showToast(
@@ -61,67 +63,82 @@ class CompanyPageState extends State<CompanyPage> {
       ),
       body: SingleChildScrollView(
         child: Column(
-        children: [
-          SizedBox(height:  15,),
-          const Text("Notre équipe :",
-            style: TextStyle(
-              color: Color.fromRGBO(70, 130, 180, 1),
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-              decoration: TextDecoration.underline,
-              decorationThickness: 2.0,
-              decorationStyle: TextDecorationStyle.solid,
-            )),
-          SizedBox(height:  70,),
-          Center(child: Wrap(
-            spacing: 70.0,
-            runSpacing: 20.0,
-            children: List.generate(
-              members.length,
-              (index) => Column(children: [
-                  Image.asset(
-                    members[index],
-                    key: Key('member_image_$index'),
-                    width: 95,
-                    height: 95,
-                ),
-                const SizedBox(height: 5,),
-                Text(
-                  members[index].substring(members[index].indexOf('/') + 1, members[index].indexOf('.')).toUpperCase(),
-                  style: const TextStyle(
-                    color: Color.fromRGBO(70, 130, 180, 1),
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold,
+          children: [
+            SizedBox(
+              height: 15,
+            ),
+            const Text("Notre équipe :",
+                style: TextStyle(
+                  color: Color.fromRGBO(70, 130, 180, 1),
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.underline,
+                  decorationThickness: 2.0,
+                  decorationStyle: TextDecorationStyle.solid,
+                )),
+            SizedBox(
+              height: 70,
+            ),
+            Center(
+              child: Wrap(
+                spacing: 70.0,
+                runSpacing: 20.0,
+                children: List.generate(
+                  members.length,
+                  (index) => Column(
+                    children: [
+                      Image.asset(
+                        members[index],
+                        key: Key('member_image_$index'),
+                        width: 95,
+                        height: 95,
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        members[index]
+                            .substring(members[index].indexOf('/') + 1,
+                                members[index].indexOf('.'))
+                            .toUpperCase(),
+                        style: const TextStyle(
+                          color: Color.fromRGBO(70, 130, 180, 1),
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
+              ),
             ),
+            SizedBox(
+              height: 80,
             ),
-          ),
-          ),
-          SizedBox(height: 80,),
-          const Text("Nos Conteneurs :",
-              style: TextStyle(
-                color: Color.fromRGBO(70, 130, 180, 1),
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                decoration: TextDecoration.underline,
-                decorationThickness: 2.0,
-                decorationStyle: TextDecorationStyle.solid,
-              )),
-          SizedBox(height: 65,),
-          Wrap(
-          spacing: 16.0,
-          runSpacing: 16.0,
-          children: List.generate(
-            containers.length,
-            (index) => ContainerCard(
-              container: containers[index],
+            const Text("Nos Conteneurs :",
+                style: TextStyle(
+                  color: Color.fromRGBO(70, 130, 180, 1),
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.underline,
+                  decorationThickness: 2.0,
+                  decorationStyle: TextDecorationStyle.solid,
+                )),
+            SizedBox(
+              height: 65,
             ),
-          ),
+            Wrap(
+              spacing: 16.0,
+              runSpacing: 16.0,
+              children: List.generate(
+                containers.length,
+                (index) => ContainerCard(
+                  container: containers[index],
+                ),
+              ),
+            ),
+          ],
         ),
-        ],
-      ),
       ),
       bottomNavigationBar: const CustomBottomNavigationBar(),
     );
