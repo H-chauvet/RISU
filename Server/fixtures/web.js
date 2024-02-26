134330bb0cb228bb775bd426b0f5d33644a75153
Louismaestre <louis.maestre@epitech.eu>
2024-02-25 17:55:25 -0800

    [UPDATE] Use prettier for the back end

--- Server/fixtures/web.js
+++ Server/fixtures/web.js
@@ -2,52 +2,62 @@ const { db } = require("../middleware/database");
 const bcrypt = require("bcrypt");
 
 exports.createFixtures = async () => {
-    try {
-      const organization = await db.Organization.create({
-        data: {
-          name: 'Risu',
-          type: 'Entreprise',
-          contactInformation: 'Contact Information Example',
-          affiliate: {
-            create: [
-              {
-                firstName: 'Louis',
-                lastName: 'Maestre',
-                email: 'louis.maestre@hotmail.fr',
-                confirmed: true,
-                password: bcrypt.hashSync("louismaestre", 12),
-              },
-            ],
-          },
-          containers: {
-            create: [
-              {
-                id: 3,
-                city: 'Paris',
-                address: '4 rue George',
-                items: {
-                  create: [
-                    { id: 1, name: 'Ballon de foot', price: 10, available: true },
-                    { id: 2, name: 'Ballon de volley', price: 20, available: false },
-                  ],
-                },
-              },
-              {
-                id: 4,
-                city: 'Nantes',
-                address: '8 rue George',
-                items: {
-                  create: [
-                    { id: 3, name: 'Ballon de foot', price: 10, available: true },
-                    { id: 4, name: 'Ballon de volley', price: 20, available: false },
-                  ],
-                },
-              },
-            ],
-          },
+  try {
+    const organization = await db.Organization.create({
+      data: {
+        name: "Risu",
+        type: "Entreprise",
+        contactInformation: "Contact Information Example",
+        affiliate: {
+          create: [
+            {
+              firstName: "Louis",
+              lastName: "Maestre",
+              email: "louis.maestre@hotmail.fr",
+              confirmed: true,
+              password: bcrypt.hashSync("louismaestre", 12),
+            },
+          ],
         },
-      });
-    } catch (err) {
-      console.error(err.message);
-    }
-  };
+        containers: {
+          create: [
+            {
+              id: 3,
+              city: "Paris",
+              address: "4 rue George",
+              items: {
+                create: [
+                  { id: 1, name: "Ballon de foot", price: 10, available: true },
+                  {
+                    id: 2,
+                    name: "Ballon de volley",
+                    price: 20,
+                    available: false,
+                  },
+                ],
+              },
+            },
+            {
+              id: 4,
+              city: "Nantes",
+              address: "8 rue George",
+              items: {
+                create: [
+                  { id: 3, name: "Ballon de foot", price: 10, available: true },
+                  {
+                    id: 4,
+                    name: "Ballon de volley",
+                    price: 20,
+                    available: false,
+                  },
+                ],
+              },
+            },
+          ],
+        },
+      },
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
+};
