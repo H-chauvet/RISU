a32b535f940581f0874d7ac7f0ec17d81cf52e1c
Nathan <nathan.rousseau@epitech.eu>
2024-03-04 17:43:22 +0100

    [ADD] Documentation example file

---
+++ Server/documentation.example.js
@@ -0,0 +1,9 @@
+/**
+ * Example function for documentation
+ *
+ * @param {*} nbr to be returned @default 42
+ * @returns the nbr given in param
+ */
+function exampleFunction(nbr = 42) {
+    return nbr;
+}
