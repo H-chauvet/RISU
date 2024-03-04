2524d4c63557ab46cd58b2f1dbb99df8bbf531e1
Nathan <nathan.rousseau@epitech.eu>
2024-03-04 18:07:17 +0100

    [ADD] Documentation on Container controller

--- Server/documentation.example.js
+++ Server/documentation.example.js
@@ -1,7 +1,7 @@
 /**
  * Example function for documentation
  *
- * @param {*} nbr to be returned @default 42
+ * @param {number} nbr to be returned @default 42
  * @returns the nbr given in param
  */
 function exampleFunction(nbr = 42) {
