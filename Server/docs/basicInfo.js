ee42ef4da19495083f2c133702f74abcff69e60b
Nathan <nathan.rousseau@epitech.eu>
2024-03-06 16:24:15 +0100

    [FIX] Typo for prettier

--- Server/docs/basicInfo.js
+++ Server/docs/basicInfo.js
@@ -1,14 +1,14 @@
 module.exports = {
   openapi: "3.1.0",
-    info: {
-      title: "Developer Documentation of Risu's API",
-      version : 0.1,
-      description:
-        "This is the official API of the RISU Project. Documented with Swagger.",
-      contact: {
-        name: "Risu",
-        url: "http://eip.epitech.eu/2025/risu",
-        email: "risu_2025@labeip.epitech.eu"
-      }
+  info: {
+    title: "Developer Documentation of Risu's API",
+    version: 0.1,
+    description:
+      "This is the official API of the RISU Project. Documented with Swagger.",
+    contact: {
+      name: "Risu",
+      url: "http://eip.epitech.eu/2025/risu",
+      email: "risu_2025@labeip.epitech.eu",
     },
+  },
 };
