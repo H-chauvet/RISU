706daa30ea164316a715dadda69fefdee3971b60
Nathan <nathan.rousseau@epitech.eu>
2024-03-06 15:28:17 +0100

    [ADD] Feedback web documentation #252

--- Server/controllers/Web/feedbacks.js
+++ Server/controllers/Web/feedbacks.js
@@ -1,22 +1,35 @@
 const { db } = require('../../middleware/database')
 
+/**
+ * Create a new feedback
+ *
+ * @param {*} data of the feedback to be created
+ * @returns the newly created feedback
+ */
 exports.registerFeedbacks = data => {
-    return db.Feedbacks_Web.create({
-      data: data
-    })
+  return db.Feedbacks_Web.create({
+    data: data
+  })
+}
+
+/**
+ * Get every feedbacks that correspond to the mark
+ * If mark is not defined, retrieve every feedbacks
+ *
+ * @param {string} mark of the feedback
+ * @returns the found feedbacks
+ */
+exports.getAllFeedbacks = async (mark) => {
+  try {
+    const filter = mark ? { mark } : {};
+
+    const feedbacks = await db.Feedbacks_Web.findMany({
+      where: filter
+    });
+
+    return feedbacks;
+  } catch (error) {
+    console.error('Error retrieving feedbacks:', error);
+    throw new Error('Failed to retrieve feedbacks');
   }
-
-  exports.getAllFeedbacks = async (mark) => {
-    try {
-      const filter = mark ? { mark } : {};
-
-      const feedbacks = await db.Feedbacks_Web.findMany({
-        where: filter
-      });
-
-      return feedbacks;
-    } catch (error) {
-      console.error('Error retrieving feedbacks:', error);
-      throw new Error('Failed to retrieve feedbacks');
-    }
-  };
+};
