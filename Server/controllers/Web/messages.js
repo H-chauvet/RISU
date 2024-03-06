c0ab1fbcc4966178e50dff7e96b591a07e0b5680
Nathan <nathan.rousseau@epitech.eu>
2024-03-06 15:30:35 +0100

    [ADD] Message web documentation #252

--- Server/controllers/Web/messages.js
+++ Server/controllers/Web/messages.js
@@ -1,5 +1,10 @@
 const { db } = require('../../middleware/database')
 
+/**
+ * Delete a message by its id
+ *
+ * @param {number} id of the message to be deleted
+ */
 exports.deleteMessageById = async (id) => {
     try {
       await db.Contact_Web.delete({
@@ -11,22 +16,33 @@ exports.deleteMessageById = async (id) => {
     }
   };
 
-  exports.getAllMessages = async () => {
-    try {
-      const messages = await db.Contact_Web.findMany();
-      return messages;
-    } catch (error) {
-      console.error('Error retrieving messages:', error);
-      throw new Error('Failed to retrieve messages');
-    }
-  };
+/**
+ * Retrieve every messages from the database
+ *
+ * @returns messages objects
+ */
+exports.getAllMessages = async () => {
+  try {
+    const messages = await db.Contact_Web.findMany();
+    return messages;
+  } catch (error) {
+    console.error('Error retrieving messages:', error);
+    throw new Error('Failed to retrieve messages');
+  }
+};
 
-  exports.findById = id => {
-    try {
-      const message = db.Contact_Web.findUnique({ where: { id } });
-      return message;
-    } catch (error) {
-      console.error('Error retrieving message by ID:', error);
-      throw new Error('Failed to retrieve message by ID');
-    }
-  };
+/**
+ * Find a message by its id
+ *
+ * @param {number} id of the message to be found
+ * @returns the message if found
+ */
+exports.findById = id => {
+  try {
+    const message = db.Contact_Web.findUnique({ where: { id } });
+    return message;
+  } catch (error) {
+    console.error('Error retrieving message by ID:', error);
+    throw new Error('Failed to retrieve message by ID');
+  }
+};
