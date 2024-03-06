6a8c9e2dc28fa9a80121d2550d968bdecf2e1e2e
Nathan <nathan.rousseau@epitech.eu>
2024-03-06 14:14:26 +0100

    [ADD] Mobile contact documentation #252

--- Server/controllers/Mobile/contact.js
+++ Server/controllers/Mobile/contact.js
@@ -1,5 +1,13 @@
 const { db } = require('../../middleware/database')
 
+/**
+ * Create a object with the message sent from a mobile user
+ *
+ * @param {string} name
+ * @param {string} email
+ * @param {string} message
+ * @returns the contact object
+ */
 exports.createContact = (name, email, message) => {
     return db.Contact_Mobile.create({
       data: {
