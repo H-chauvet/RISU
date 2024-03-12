3c75474b2ae74215dd1b266be65d2594fe048fe8
Nathan <nathan.rousseau@epitech.eu>
2024-03-12 17:41:57 +0100

    [FIX] Typo on odcumentation example #252

--- Server/documentation.example.js
+++ Server/documentation.example.js
@@ -2,7 +2,7 @@
  * Example function for documentation
  *
  * @param {number} nbr to be returned @default 42
- * @returns the nbr given in param
+ * @returns the number given in param
  */
 function exampleFunction(nbr = 42) {
     return nbr;
