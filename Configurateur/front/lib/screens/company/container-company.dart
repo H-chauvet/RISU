9f736b359fb4ddc0f8ca88f160f6390b17f2a5e3
Nathan <nathan.rousseau@epitech.eu>
2024-03-24 16:12:34 +0100

    [FIX] Typo for prettier #254

--- Configurateur/front/lib/screens/company/container-company.dart
+++ Configurateur/front/lib/screens/company/container-company.dart
@@ -6,7 +6,7 @@ import 'package:front/services/storage_service.dart';
 import 'package:front/services/theme_service.dart';
 import 'package:front/styles/themes.dart';
 import 'package:go_router/go_router.dart';
-import 'package:front/components/custom_app_bar.dart';;
+import 'package:front/components/custom_app_bar.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
