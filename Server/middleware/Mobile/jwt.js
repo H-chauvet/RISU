5ef5d9b2e80f5fe33bc32573e4e0d1301962942f
BuildTools <hugo.lopes-pinto@epitech.eu>
2024-04-06 15:46:57 +0200

    [ADD] Function for the refreshToken

--- Server/middleware/Mobile/jwt.js
+++ Server/middleware/Mobile/jwt.js
@@ -27,7 +27,37 @@ function generateRefreshToken(id) {
   return crypto.randomBytes(64).toString('hex');
 }
 
+/**
+ * Middleware to refresh token if expired
+ *
+ * @param {*} req Express request object
+ * @param {*} res Express response object
+ * @param {*} next Next middleware function
+ */
+const refreshTokenMiddleware = async (req, res, next) => {
+  try {
+    if (!req.headers.authorization) {
+      return next();
+    }
+    const token = req.headers.authorization.split(' ')[1];
+    const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+
+    const currentTimestamp = Math.floor(Date.now() / 1000);
+    if (decoded.exp < currentTimestamp) {
+      const userId = decoded.id;
+      const newToken = generateToken(userId);
+
+      res.setHeader('Authorization', `Bearer ${newToken}`);
+    }
+    next();
+  } catch (err) {
+    console.error('Error in refreshTokenMiddleware:', err.message);
+    next(err);
+  }
+};
+
 module.exports = {
   generateToken,
-  generateRefreshToken
+  generateRefreshToken,
+  refreshTokenMiddleware
 };
