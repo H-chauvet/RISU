ab35fe5420914f45c92de60d089b9411cb6b77f9
Quentin Leroueil <quentin.leroueil@epitech.eu>
2024-04-15 18:52:25 +0200

    [ADD] server side
    routes to:
    - create a favorite
    - get all favorite of a user
    - get a specific favorite from an item
    - delete a favorite

---
+++ Server/controllers/Mobile/favorites.js
@@ -0,0 +1,97 @@
+const { db } = require('../../middleware/database')
+
+/**
+ * Create a favorite item for the specified user
+ * 
+ * @param {*} userId that added the item
+ * @param {*} itemId to be added
+ * @returns the newly created favorite
+ */
+exports.createFavoriteItem = (userId, itemId) => {
+  return db.Favorite_Article_Mobile.create({
+    data: {
+      userId: userId,
+      itemId: itemId,
+    }
+  })
+}
+
+/**
+ * Get all the favorites items related to a userId
+ * 
+ * @param {*} userId of the related user
+ * @returns all the favorites of the user
+ */
+exports.getUserFavorites = (userId) => {
+  return db.Favorite_Article_Mobile.findMany({
+    where: { userId: userId },
+    select: {
+      id: true,
+      item: {
+        select: {
+          id: true,
+          name: true,
+          available: true,
+        }
+      }
+    }
+  })
+}
+
+/**
+ * Get a favorite item from the related item and user
+ * 
+ * @param {*} userId of the user
+ * @param {*} itemId of the item
+ * @returns the related favorite
+ */
+exports.getItemFavorite = (userId, itemId) => {
+  return db.Favorite_Article_Mobile.findFirst({
+    where: {
+      AND: [
+        { userId: userId },
+        { itemId: itemId },
+      ]
+    },
+    select: {
+      id: true,
+      item: {
+        select: {
+          id: true,
+          name: true,
+          available: true,
+        }
+      }
+    }
+  })
+}
+
+/**
+ * Check if a user already put an item as favorite
+ * 
+ * @param {*} userId of the user
+ * @param {*} itemId of the item
+ * @returns a boolean depending if the item is already as favorite for the user
+ */
+exports.checkFavorite = (userId, itemId) => {
+  return db.Favorite_Article_Mobile.count({
+    where: {
+      AND: [
+        { userId: userId },
+        { itemId: itemId },
+      ]
+    }
+  }).then(Boolean)
+}
+
+/**
+ * delete a specific favorite item from its id
+ * 
+ * @param {*} favoriteId of the related favorite item
+ * @returns none
+ */
+exports.deleteFavorite = (favoriteId) => {
+  return db.Favorite_Article_Mobile.delete({
+    where: { id: favoriteId },
+  })
+}
