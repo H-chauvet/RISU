28358c77963b76a80752aa774247f14a712d7884
Louismaestre <louis.maestre@epitech.eu>
2024-04-16 14:43:40 -0700

    [UPDATE] add information.dart

---
+++ Configurateur/front/lib/network/informations.dart
@@ -0,0 +1,5 @@
+// Variable globale pour stocker l'adresse IP du serveur
+
+//String? serverIp = 'localhost'; // A garder en local
+
+String? serverIp = '51.11.241.159'; // A mettre quand push github
