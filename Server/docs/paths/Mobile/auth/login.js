97ab009738f05833c6ecdd0d841fc851f3dcc1c8
BuildTools <hugo.lopes-pinto@epitech.eu>
2024-04-16 11:51:07 +0200

    [UPDATE] String to boolean

--- Server/docs/paths/Mobile/auth/login.js
+++ Server/docs/paths/Mobile/auth/login.js
@@ -18,7 +18,7 @@ module.exports = {
           name: 'longTerm',
           in: 'body',
           schema: {
-            type: 'string',
+            type: 'boolean',
             example: 'true'
           }
         }
