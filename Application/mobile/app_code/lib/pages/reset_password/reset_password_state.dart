c417781b8f19c4408b365fbacf2e62d95a066fbc
tbellicha <tanguy.bellicha@epitech.eu>
2024-05-05 23:46:32 -0700

    [ADD] UpdatePassword function

--- Application/mobile/app_code/lib/pages/reset_password/reset_password_state.dart
+++ Application/mobile/app_code/lib/pages/reset_password/reset_password_state.dart
@@ -1,11 +1,18 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
+import 'package:risu/components/alert_dialog.dart';
 import 'package:risu/components/appbar.dart';
 import 'package:risu/components/filled_button.dart';
 import 'package:risu/components/loader.dart';
 import 'package:risu/components/text_input.dart';
+import 'package:risu/components/toast.dart';
+import 'package:risu/globals.dart';
 import 'package:risu/pages/reset_password/reset_password_page.dart';
+import 'package:risu/utils/errors.dart';
 import 'package:risu/utils/providers/theme.dart';
 
 class ResetPasswordPageState extends State<ResetPasswordPage> {
@@ -14,6 +21,84 @@ class ResetPasswordPageState extends State<ResetPasswordPage> {
   bool _isPasswordConfirmationVisible = false;
   String _password = '';
   String _passwordConfirmation = '';
+
+  Future<void> updatePassword() async {
+    try {
+      if (_password == '' || _passwordConfirmation == '') {
+        if (mounted) {
+          await MyAlertDialog.showErrorAlertDialog(
+            context: context,
+            title: AppLocalizations.of(context)!.error,
+            message: AppLocalizations.of(context)!.fieldsEmpty,
+          );
+        }
+        return;
+      }
+      if (_password != _passwordConfirmation) {
+        if (mounted) {
+          await MyAlertDialog.showErrorAlertDialog(
+            context: context,
+            title: AppLocalizations.of(context)!.error,
+            message: AppLocalizations.of(context)!.passwordsDoNotMatch,
+          );
+        }
+        return;
+      }
+
+      final token = widget.token;
+      setState(() {
+        _loaderManager.setIsLoading(true);
+      });
+      final response = await http.put(
+        // TODO Change URL to the correct one
+        Uri.parse('$baseUrl/api/mobile/user/password'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Authorization': 'Bearer $token',
+        },
+        body: jsonEncode({
+          'newPassword': _password,
+        }),
+      );
+      setState(() {
+        _loaderManager.setIsLoading(false);
+      });
+      if (response.statusCode == 200) {
+        json.decode(response.body);
+        if (mounted) {
+          MyToastMessage.show(
+            context: context,
+            message: AppLocalizations.of(context)!.passwordUpdated,
+          );
+        }
+      } else {
+        if (response.statusCode == 401) {
+          if (mounted) {
+            printServerResponse(context, response, 'updatePassword',
+                message:
+                    AppLocalizations.of(context)!.passwordCurrentIncorrect);
+          }
+        } else {
+          if (mounted) {
+            printServerResponse(context, response, 'updatePassword',
+                message: AppLocalizations.of(context)!
+                    .errorOccurredDuringPasswordUpdate);
+          }
+        }
+      }
+    } catch (err, stacktrace) {
+      if (mounted) {
+        setState(() {
+          _loaderManager.setIsLoading(false);
+        });
+        printCatchError(context, err, stacktrace,
+            message:
+                AppLocalizations.of(context)!.errorOccurredDuringSavingData);
+        return;
+      }
+      return;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -87,7 +172,7 @@ class ResetPasswordPageState extends State<ResetPasswordPage> {
       floatingActionButton: MyButton(
         key: const Key('reset_password-confirm'),
         text: AppLocalizations.of(context)!.confirm,
-        onPressed: () {},
+        onPressed: updatePassword,
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
