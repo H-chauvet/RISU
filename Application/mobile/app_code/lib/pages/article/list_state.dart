76870914cb57715c9aa0c603ead51b9655fb8d1c
BuildTools <hugo.lopes-pinto@epitech.eu>
2024-05-06 18:24:01 +0200

    [REMOVE] Commented lines

--- Application/mobile/app_code/lib/pages/article/list_state.dart
+++ Application/mobile/app_code/lib/pages/article/list_state.dart
@@ -193,7 +193,6 @@ class ArticleListState extends State<ArticleListPage> {
                                       ),
                                     ),
                                   ).then((filters) {
-                                    // Handle returned filters here
                                     if (filters != null) {
                                       setState(() {
                                         isAscending = filters['isAscending'];
