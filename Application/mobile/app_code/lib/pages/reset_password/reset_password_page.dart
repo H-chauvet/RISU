2059887ad5279b9d7e0670d5580361608c49872b
tbellicha <tanguy.bellicha@epitech.eu>
2024-05-05 23:47:22 -0700

    [ADD] Token as parameter of the page

--- Application/mobile/app_code/lib/pages/reset_password/reset_password_page.dart
+++ Application/mobile/app_code/lib/pages/reset_password/reset_password_page.dart
@@ -3,8 +3,11 @@ import 'package:flutter/material.dart';
 import 'reset_password_state.dart';
 
 class ResetPasswordPage extends StatefulWidget {
+  final String token;
+
   const ResetPasswordPage({
     super.key,
+    required this.token,
   });
 
   @override
