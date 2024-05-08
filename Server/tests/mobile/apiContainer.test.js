9c4dc615b97065822c329db496fa6c724357393a
BuildTools <hugo.lopes-pinto@epitech.eu>
2024-05-08 17:18:15 +0200

    [ADD] New tests for getting articles with filters

--- Server/tests/mobile/apiContainer.test.js
+++ Server/tests/mobile/apiContainer.test.js
@@ -72,16 +72,64 @@ describe('GET /api/mobile/container/articleslist/', () => {
       done
     )
   }),
-    it('should get article list detais from empty container', (done) => {
-      async.series(
-        [
-          async function () {
-            const res = await request('http://localhost:3000')
-              .get(`/api/mobile/container/${containerId[1]}/articleslist`)
-            expect(res.statusCode).toBe(200)
-          }
-        ],
-        done
-      )
-    })
+  it('should get article list detais from empty container, without filters', (done) => {
+    async.series(
+      [
+        async function () {
+          const res = await request('http://localhost:3000')
+            .get(`/api/mobile/container/${containerId[1]}/articleslist`)
+          expect(res.statusCode).toBe(200)
+        }
+      ],
+      done
+    )
+  }),
+  it('should get article list detais from empty container, with defaults filters', (done) => {
+    async.series(
+      [
+        async function () {
+          const res = await request('http://localhost:3000')
+            .get('/api/mobile/container/1/articleslist?articleName=&isAscending=true&isAvailable=true&categoryId=null&sortBy=price')
+          expect(res.statusCode).toBe(200)
+        }
+      ],
+      done
+    )
+  }),
+  it('should get article list detais from empty container, with filters', (done) => {
+    async.series(
+      [
+        async function () {
+          const res = await request('http://localhost:3000')
+            .get('/api/mobile/container/1/articleslist?articleName=&isAscending=false&isAvailable=true&categoryId=1&sortBy=price')
+          expect(res.statusCode).toBe(200)
+        }
+      ],
+      done
+    )
+  }),
+  it('should get article list detais from empty container, with filters', (done) => {
+    async.series(
+      [
+        async function () {
+          const res = await request('http://localhost:3000')
+            .get('/api/mobile/container/1/articleslist?articleName=test&isAscending=true&isAvailable=false&categoryId=1&sortBy=rating')
+          expect(res.statusCode).toBe(200)
+        }
+      ],
+      done
+    )
+  }),
+  it('should get article list detais from empty container, with incorrect filters', (done) => {
+    async.series(
+      [
+        async function () {
+          const res = await request('http://localhost:3000')
+            .get('/api/mobile/container/1/articleslist?articleName=&isAscending=true&isAvailable=false&categoryId=test&sortBy=rating')
+          expect(res.statusCode).toBe(401)
+        }
+      ],
+      done
+    )
+  })
 })
