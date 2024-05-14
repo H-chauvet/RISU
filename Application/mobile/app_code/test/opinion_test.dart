9c624fb7894766170e74fbd8e9e7409c086dd9e7
BuildTools <hugo.lopes-pinto@epitech.eu>
2024-05-14 16:53:41 +0200

    [FIX] Test for flutter upgrade

--- Application/mobile/app_code/test/opinion_test.dart
+++ Application/mobile/app_code/test/opinion_test.dart
@@ -215,7 +215,7 @@ void main() {
       while (true) {
         try {
           expect(find.byType(CircularProgressIndicator), findsOneWidget);
-          await tester.pumpWidget(testPage, const Duration(milliseconds: 100));
+          await tester.pumpWidget(testPage);
         } catch (e) {
           break;
         }
