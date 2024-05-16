83e8a98dbf2dd6a691fb1546bb651f6847fa81a7
BuildTools <hugo.lopes-pinto@epitech.eu>
2024-05-16 19:05:03 +0200

    [UPDATE] Email update

--- Application/mobile/app_code/lib/pages/profile/informations/informations_state.dart
+++ Application/mobile/app_code/lib/pages/profile/informations/informations_state.dart
@@ -82,6 +82,78 @@ class ProfileInformationsPageState extends State<ProfileInformationsPage> {
     }
   }
 
+  Future<void> updateEmail() async {
+    try {
+      final token = userInformation!.token;
+      Map<String, dynamic> body = {};
+      if (newEmail != '') {
+        if (Validators().email(context, newEmail) != null) {
+          if (mounted) {
+            await MyAlertDialog.showErrorAlertDialog(
+              context: context,
+              title: AppLocalizations.of(context)!.error,
+              message: AppLocalizations.of(context)!.emailInvalid,
+            );
+          }
+          return;
+        }
+        body['newEmail'] = newEmail;
+      } else {
+        if (mounted) {
+          await MyAlertDialog.showErrorAlertDialog(
+            context: context,
+            title: AppLocalizations.of(context)!.error,
+            message: AppLocalizations.of(context)!.fieldsEmpty,
+          );
+        }
+        return;
+      }
+      setState(() {
+        _loaderManager.setIsLoading(true);
+      });
+      final response = await http.put(
+        Uri.parse('$baseUrl/api/mobile/user/newEmail'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Authorization': 'Bearer $token',
+        },
+        body: jsonEncode(body),
+      );
+      setState(() {
+        _loaderManager.setIsLoading(false);
+      });
+      if (response.statusCode == 200) {
+        json.decode(response.body);
+        if (mounted) {
+          await fetchUserData(context);
+        }
+        if (mounted) {
+          MyToastMessage.show(
+            context: context,
+            message: AppLocalizations.of(context)!.profileUpdated,
+          );
+        }
+      } else {
+        if (mounted) {
+          printServerResponse(context, response, 'updateUser',
+              message: AppLocalizations.of(context)!
+                  .errorOccurredDuringUpdateUserInformation);
+        }
+      }
+    } catch (err, stacktrace) {
+      if (mounted) {
+        setState(() {
+          _loaderManager.setIsLoading(false);
+        });
+        printCatchError(context, err, stacktrace,
+            message:
+                AppLocalizations.of(context)!.errorOccurredDuringSavingData);
+        return;
+      }
+      return;
+    }
+  }
+
   Future<void> updateUser() async {
     try {
       final token = userInformation!.token;
@@ -323,6 +395,28 @@ class ProfileInformationsPageState extends State<ProfileInformationsPage> {
                           setState(() => newLastName = value);
                         },
                       ),
+                      const SizedBox(height: 16),
+                      MyButton(
+                        key: const Key('profile_info-button_update'),
+                        text: AppLocalizations.of(context)!.saveChanges,
+                        onPressed: () async {
+                          await updateUser();
+                        },
+                      ),
+                      const SizedBox(height: 16),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          AppLocalizations.of(context)!.email,
+                          key: const Key('profile_info-text_informations'),
+                          style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: context.select(
+                                  (ThemeProvider themeProvider) =>
+                                      themeProvider.currentTheme.primaryColor)),
+                        ),
+                      ),
                       buildField(
                         AppLocalizations.of(context)!.email,
                         key: const Key('profile_info-text_field_email'),
@@ -333,10 +427,10 @@ class ProfileInformationsPageState extends State<ProfileInformationsPage> {
                       ),
                       const SizedBox(height: 16),
                       MyButton(
-                        key: const Key('profile_info-button_update'),
-                        text: AppLocalizations.of(context)!.saveChanges,
+                        key: const Key('profile_email-button_update'),
+                        text: AppLocalizations.of(context)!.saveEmail,
                         onPressed: () async {
-                          await updateUser();
+                          await updateEmail();
                         },
                       ),
                       const SizedBox(height: 16),
@@ -354,6 +448,7 @@ class ProfileInformationsPageState extends State<ProfileInformationsPage> {
                           ),
                         ),
                       ),
+                      const SizedBox(height: 16),
                       buildField(
                         AppLocalizations.of(context)!.passwordCurrent,
                         key: const Key(
