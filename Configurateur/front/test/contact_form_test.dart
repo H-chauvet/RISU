4c2e897d0538c479589c4c1037d75e8196c8dd22
Nathan <nathan.rousseau@epitech.eu>
2024-05-17 15:33:54 +0200

    [FIX] Bad conflitcs : begin of tests

---
+++ Configurateur/front/test/contact_form_test.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:front/app_routes.dart';
+import 'package:front/screens/contact/contact.dart';
+import 'package:front/services/theme_service.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:sizer/sizer.dart';
+
+void main() {
+  setUp(() async {
+    final roboto = rootBundle.load('assets/roboto/Roboto-Medium.ttf');
+    final fontLoader = FontLoader('Roboto')..addFont(roboto);
+    await fontLoader.load();
+  });
+
+  testWidgets('no data', (WidgetTester tester) async {
+    await tester.binding.setSurfaceSize(const Size(1920, 1080));
+
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider<ThemeService>(
+            create: (_) => ThemeService(),
+          ),
+        ],
+        child: Sizer(
+          builder: (context, orientation, deviceType) {
+            return MaterialApp(
+              theme: ThemeData(fontFamily: 'Roboto'),
+              home: InheritedGoRouter(
+                goRouter: AppRouter.router,
+                child: const MaterialApp(
+                  home: ContactPage(),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+
+    await tester.tap(find.text('Envoyer'));
+    await tester.pump();
+
+    expect(find.text('Veuillez entrer votre prénom'), findsOneWidget);
+    expect(find.text('Veuillez entrer votre nom'), findsOneWidget);
+    expect(find.text('Veuillez entrer votre email'), findsOneWidget);
+    expect(find.text('Veuillez entrer votre message'), findsOneWidget);
+    await tester.binding.setSurfaceSize(null);
+  });
+
+  testWidgets('correct data', (WidgetTester tester) async {
+    await tester.binding.setSurfaceSize(const Size(1920, 1080));
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider<ThemeService>(
+            create: (_) => ThemeService(),
+          ),
+        ],
+        child: Sizer(
+          builder: (context, orientation, deviceType) {
+            return MaterialApp(
+              theme: ThemeData(fontFamily: 'Roboto'),
+              home: InheritedGoRouter(
+                goRouter: AppRouter.router,
+                child: const MaterialApp(
+                  home: ContactPage(),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+
+    await tester.enterText(find.byType(TextFormField).at(0), 'TestPrénom');
+    await tester.enterText(find.byType(TextFormField).at(1), 'TestNom');
+    await tester.enterText(find.byType(TextFormField).at(2), 'test@test.com');
+    await tester.enterText(find.byType(TextFormField).at(3), 'TestMessage');
+    await tester.tap(find.text('Envoyer'));
+    await tester.pump();
+
+    expect(find.text('Veuillez entrer un email valide'), findsNothing);
+    expect(find.text('Veuillez entrer votre prénom'), findsNothing);
+    expect(find.text('Veuillez entrer votre nom'), findsNothing);
+    expect(find.text('Veuillez entrer votre message'), findsNothing);
+    await tester.binding.setSurfaceSize(null);
+  });
+
+  testWidgets('correct data bis', (WidgetTester tester) async {
+    await tester.binding.setSurfaceSize(const Size(1920, 1080));
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider<ThemeService>(
+            create: (_) => ThemeService(),
+          ),
+        ],
+        child: Sizer(
+          builder: (context, orientation, deviceType) {
+            return MaterialApp(
+              theme: ThemeData(fontFamily: 'Roboto'),
+              home: InheritedGoRouter(
+                goRouter: AppRouter.router,
+                child: const MaterialApp(
+                  home: ContactPage(),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+
+    await tester.enterText(find.byType(TextFormField).at(0), 'TestPrénom');
+    await tester.enterText(find.byType(TextFormField).at(1), 'TestNom');
+    await tester.enterText(find.byType(TextFormField).at(2), 'test@tes');
+    await tester.enterText(find.byType(TextFormField).at(3), 'TestMessage');
+    await tester.tap(find.text('Envoyer'));
+    await tester.pump();
+
+    expect(find.text('Veuillez entrer un email valide'), findsOneWidget);
+    expect(find.text('Veuillez entrer votre prénom'), findsNothing);
+    expect(find.text('Veuillez entrer votre nom'), findsNothing);
+    expect(find.text('Veuillez entrer votre message'), findsNothing);
+    await tester.binding.setSurfaceSize(null);
+  });
+}
--- Configurateur/front/test/container-creation/recap_screen_test.dart
+++ Configurateur/front/test/container-creation/recap_screen_test.dart
@@ -6,48 +6,60 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:front/app_routes.dart';
-import 'package:front/screens/container-creation/recap_screen.dart';
+import 'package:front/screens/container-creation/recap_screen/recap_screen.dart';
 import 'package:front/services/storage_service.dart';
 import 'package:front/services/theme_service.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mockito/mockito.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sizer/sizer.dart';
 
 void main() {
   late MockSharedPreferences sharedPreferences;
 
-  setUp(() {
+  setUp(() async {
     sharedPreferences = MockSharedPreferences();
+    final roboto = rootBundle.load('assets/roboto/Roboto-Medium.ttf');
+    final fontLoader = FontLoader('Roboto')..addFont(roboto);
+    await fontLoader.load();
   });
 
   testWidgets('Recap screen', (WidgetTester tester) async {
-    tester.binding.window.physicalSizeTestValue = const Size(5000, 5000);
+    tester.binding.window.physicalSizeTestValue = const Size(1920, 1080);
     tester.binding.window.devicePixelRatioTestValue = 1.0;
 
     when(sharedPreferences.getString('token')).thenReturn('test-token');
 
-    await tester.pumpWidget(MultiProvider(
-      providers: [
-        ChangeNotifierProvider<ThemeService>(
-          create: (_) => ThemeService(),
-        ),
-      ],
-      child: MaterialApp(
-        home: InheritedGoRouter(
-          goRouter: AppRouter.router,
-          child: const RecapScreen(
-            lockers:
-                '[{"type":"Petit casier","price":10},{"type":"Moyen casier","price":20},{"type":"Grand casier","price":30}]',
-            amount: 60,
-            containerMapping: '1',
-            id: '1',
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider<ThemeService>(
+            create: (_) => ThemeService(),
           ),
+        ],
+        child: Sizer(
+          builder: (context, orientation, deviceType) {
+            return MaterialApp(
+              theme: ThemeData(fontFamily: 'Roboto'),
+              home: InheritedGoRouter(
+                goRouter: AppRouter.router,
+                child: const RecapScreen(
+                  lockers:
+                      '[{"type":"Petit casier","price":10},{"type":"Moyen casier","price":20},{"type":"Grand casier","price":30}]',
+                  amount: 60,
+                  containerMapping: '1',
+                  id: '1',
+                ),
+              ),
+            );
+          },
         ),
       ),
-    ));
+    );
 
     await tester.pumpAndSettle(const Duration(seconds: 2));
 
