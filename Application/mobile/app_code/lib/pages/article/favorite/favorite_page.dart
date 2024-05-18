71e6be57a01d7833414cdefa6aac34fcddded314
BuildTools <hugo.lopes-pinto@epitech.eu>
2024-05-18 02:35:26 +0200

    [ADD] keys and prepare for tests

--- Application/mobile/app_code/lib/pages/article/favorite/favorite_page.dart
+++ Application/mobile/app_code/lib/pages/article/favorite/favorite_page.dart
@@ -3,8 +3,11 @@ import 'package:flutter/material.dart';
 import 'favorite_state.dart';
 
 class FavoritePage extends StatefulWidget {
+  final List<dynamic> favorites;
+
   const FavoritePage({
     super.key,
+    this.favorites = const [],
   });
 
   @override
