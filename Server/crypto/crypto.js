e20c1e6d2bf70dddd65e843b87d85ceaa537d213
BuildTools <hugo.lopes-pinto@epitech.eu>
2024-05-19 13:39:09 +0200

    [ADD] Run prettier back

--- Server/crypto/crypto.js
+++ Server/crypto/crypto.js
@@ -1,7 +1,7 @@
-const CryptoJS = require('crypto-js');
+const CryptoJS = require("crypto-js");
 
-const key = CryptoJS.enc.Hex.parse('0123456789abcdef0123456789abcdef');
-const iv = CryptoJS.enc.Hex.parse('abcdef9876543210abcdef9876543210');
+const key = CryptoJS.enc.Hex.parse("0123456789abcdef0123456789abcdef");
+const iv = CryptoJS.enc.Hex.parse("abcdef9876543210abcdef9876543210");
 
 function encrypt(text) {
   const encrypted = CryptoJS.AES.encrypt(text, key, { iv: iv }).toString();
