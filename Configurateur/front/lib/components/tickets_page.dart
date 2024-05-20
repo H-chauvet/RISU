cd72a0ad39abdf8f9a1e7adcd084e8d590db2fdb
Henri <henri.chauvet@epitech.eu>
2024-05-20 14:17:42 +0200

    [UPDATE] Responsive for conversation on ticket page

--- Configurateur/front/lib/components/tickets_page.dart
+++ Configurateur/front/lib/components/tickets_page.dart
@@ -754,8 +754,8 @@ class TicketsState extends State<TicketsPage> {
                           ),
                           const SizedBox(height: 38),
                           Container(
-                            width: double.infinity,
-                            height: 400,
+                            width: MediaQuery.of(context).size.width * 0.5,
+                            height: MediaQuery.of(context).size.height * 0.5,
                             decoration: BoxDecoration(
                               borderRadius: BorderRadius.circular(30.0),
                               border: Border.all(color: Colors.grey),
