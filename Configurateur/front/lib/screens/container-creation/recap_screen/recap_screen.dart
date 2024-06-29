1349f644238308043156bb5673624d9a264e6a3c
Cédric <cedric.corge@epitech.eu>
2024-06-29 20:27:03 +0200

    [REMOVE] Useless files
    [FIX] Merge conflict

--- Configurateur/front/lib/screens/container-creation/recap_screen/recap_screen.dart
+++ Configurateur/front/lib/screens/container-creation/recap_screen/recap_screen.dart
@@ -4,14 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:front/components/custom_app_bar.dart';
 import 'package:front/components/progress_bar.dart';
 import 'package:front/components/recap_panel/recap_panel.dart';
-import 'package:front/screens/container-creation/recap_screen/recap_screen_style.dart';
 import 'package:front/services/size_service.dart';
 import 'package:front/services/storage_service.dart';
 import 'package:front/services/theme_service.dart';
 import 'package:front/styles/globalStyle.dart';
 import 'package:front/styles/themes.dart';
 import 'package:go_router/go_router.dart';
-import 'package:provider/provider.dart';
 
 /// RecapScreen
 ///
@@ -134,107 +132,13 @@ class RecapScreenState extends State<RecapScreen> {
       ),
       body: Center(
         child: FractionallySizedBox(
-          widthFactor: 0.5,
-          heightFactor: 0.7,
-          child: Container(
-            alignment: Alignment.center,
-            decoration: Provider.of<ThemeService>(context).isDark
-                ? boxDecorationDarkTheme
-                : boxDecorationLightTheme,
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  "Récapitulatif de la commande",
-                  style: TextStyle(
-                      color: Provider.of<ThemeService>(context).isDark
-                          ? darkTheme.primaryColor
-                          : lightTheme.primaryColor,
-                      fontSize: screenFormat == ScreenFormat.desktop
-                          ? desktopFontSize
-                          : tabletFontSize,
-                      fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                SizedBox(
-                  width: screenFormat == ScreenFormat.desktop
-                      ? desktopLineWidth
-                      : tabletLineWidth,
-                  child: const Divider(
-                    color: Colors.grey,
-                    height: 20,
-                    thickness: 1,
-                    indent: 20,
-                    endIndent: 20,
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                FractionallySizedBox(
-                  widthFactor: 0.3,
-                  child: ListView.builder(
-                    scrollDirection: Axis.vertical,
-                    shrinkWrap: true,
-                    itemCount: lockerss.length,
-                    itemBuilder: (_, i) {
-                      return Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(left: 10, bottom: 10),
-                            child: Text(
-                              lockerss[i].type,
-                            ),
-                          ),
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(right: 10, bottom: 10),
-                            child: Text(
-                              "${lockerss[i].price.toString()}€",
-                            ),
-                          ),
-                        ],
-                      );
-                    },
-                  ),
-                ),
-                SizedBox(
-                  width: screenFormat == ScreenFormat.desktop
-                      ? desktopLineWidth
-                      : tabletLineWidth,
-                  child: const Divider(
-                    color: Colors.grey,
-                    height: 20,
-                    thickness: 1,
-                    indent: 20,
-                    endIndent: 20,
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  "Prix total: ${widget.amount}€",
-                  style: TextStyle(
-                      color: Provider.of<ThemeService>(context).isDark
-                          ? darkTheme.primaryColor
-                          : lightTheme.primaryColor,
-                      fontSize: screenFormat == ScreenFormat.desktop
-                          ? desktopFontSize
-                          : tabletFontSize,
-                      fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-          ),
-        ),
+            widthFactor: 0.4,
+            heightFactor: 0.5,
+            child: RecapPanel(
+              articles: lockerss,
+              screenFormat: screenFormat,
+              fullscreen: true,
+            )),
       ),
     );
   }
