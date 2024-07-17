99846ddc6e807f0e6adb3bc7bdae14303a80966a
Henri <henri.chauvet@epitech.eu>
2024-07-17 21:55:51 +0200

    [UPDATE] Changing a bit of UI

--- Configurateur/front/lib/components/custom_toast.dart
+++ Configurateur/front/lib/components/custom_toast.dart
@@ -2,29 +2,49 @@ import 'package:flutter/material.dart';
 
 void showCustomToast(BuildContext context, String message, bool isSuccessful) {
   final overlay = Overlay.of(context);
-  final overlayEntry = OverlayEntry(
+  late OverlayEntry overlayEntry;
+  bool isOverlayEntryActive = true;
+
+  overlayEntry = OverlayEntry(
     builder: (context) => Positioned(
       top: 50,
       right: 10.0,
       child: Material(
         color: Colors.transparent,
-        child: CustomToast(message: message, isSuccessful: isSuccessful),
+        child: CustomToast(
+          message: message,
+          isSuccessful: isSuccessful,
+          onClose: () {
+            if (isOverlayEntryActive) {
+              overlayEntry.remove();
+              isOverlayEntryActive = false;
+            }
+          },
+        ),
       ),
     ),
   );
+
   overlay.insert(overlayEntry);
   Future.delayed(const Duration(seconds: 3), () {
-    overlayEntry.remove();
+    if (isOverlayEntryActive) {
+      overlayEntry.remove();
+      isOverlayEntryActive = false;
+    }
   });
 }
 
 class CustomToast extends StatefulWidget {
   final String message;
   final bool isSuccessful;
+  final VoidCallback onClose;
 
-  const CustomToast(
-      {Key? key, required this.message, required this.isSuccessful})
-      : super(key: key);
+  const CustomToast({
+    Key? key,
+    required this.message,
+    required this.isSuccessful,
+    required this.onClose,
+  }) : super(key: key);
 
   @override
   CustomToastState createState() => CustomToastState();
@@ -68,56 +88,67 @@ class CustomToastState extends State<CustomToast>
                 : const Color(0xFFF08F74),
             borderRadius: BorderRadius.circular(16.0),
           ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(widget.isSuccessful ? Icons.check : Icons.close,
-                      color: Colors.black),
-                  const SizedBox(width: 12.0),
-                  Expanded(
-                    child: Text(
-                      widget.isSuccessful ? "Succès !" : "Echec !",
-                      style: TextStyle(
+              Padding(
+                padding: const EdgeInsets.only(right: 12.0),
+                child: Icon(
+                  widget.isSuccessful ? Icons.check : Icons.error,
+                  color: Colors.black,
+                  size: 30.0,
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      widget.isSuccessful ? "Succès !" : "Erreur !",
+                      style: const TextStyle(
                         color: Colors.black,
                         fontSize: 14.0,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                  )
-                ],
+                    const SizedBox(height: 8.0),
+                    Text(
+                      widget.message,
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 12.0,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              const SizedBox(height: 8.0),
-              Padding(
-                padding: const EdgeInsets.only(left: 36.0),
-                child: Text(
-                  widget.message,
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 12.0,
-                  ),
+              GestureDetector(
+                onTap: widget.onClose,
+                child: const Icon(
+                  Icons.close,
+                  color: Colors.black,
+                  size: 16.0,
                 ),
               ),
             ],
           ),
         ),
         Positioned(
-            bottom: 0,
-            left: 0,
-            child: AnimatedBuilder(
-              animation: _widthAnimation,
-              builder: (context, child) {
-                return Container(
-                  width: toastWidth * _widthAnimation.value,
-                  height: 5.0,
-                  color: Colors.white,
-                );
-              },
-            ))
+          bottom: 0,
+          left: 0,
+          child: AnimatedBuilder(
+            animation: _widthAnimation,
+            builder: (context, child) {
+              return Container(
+                width: toastWidth * _widthAnimation.value,
+                height: 5.0,
+                color: Colors.white,
+              );
+            },
+          ),
+        ),
       ],
     );
   }
