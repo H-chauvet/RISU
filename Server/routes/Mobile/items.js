db62898fe30a2911bb9263e50949db46168a2957
tbellicha <tanguy.bellicha@epitech.eu>
2024-08-20 17:09:11 +0200

    [ADD] Image URL in the item getters

--- Server/routes/Mobile/items.js
+++ Server/routes/Mobile/items.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const itemCtrl = require('../../controllers/Common/items')
+const imagesCtrl = require('../../controllers/Common/images')
 
 router.get('/listAll', async (req, res, next) => {
   try {
@@ -19,6 +20,8 @@ router.get('/:articleId', async (req, res) => {
     if (!article) {
       return res.status(401).json("article not found")
     }
+    const images = await imagesCtrl.getItemImagesUrl(article.id);
+    article.imageUrl = images;
     return res.status(200).json(article)
   } catch (err) {
     console.error(err.message)
@@ -35,6 +38,10 @@ router.get('/:articleId/similar', async (req, res, next) => {
     const articles = await itemCtrl.getSimilarItems(parseInt(req.params.articleId), parseInt(containerId))
     if (!articles) {
       return res.status(401).json("article not found")
+    }
+    for (const article of articles) {
+      const imageUrl = await imagesCtrl.getItemImagesUrl(article.id, 0);
+      article.imageUrl = imageUrl[0];
     }
     return res.status(200).json(articles)
   } catch (err) {
