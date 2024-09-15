d5293c7e55d9fc568ea0516069a861015ae2fbc6
Henri Chauvet <henri.chauvet@epitech.eu>
2024-09-15 19:52:05 +0200

    Update informations.dart

--- Configurateur/front/lib/network/informations.dart
+++ Configurateur/front/lib/network/informations.dart
@@ -4,6 +4,6 @@ import 'package:flutter/foundation.dart';
 
 /// [serverIp] : Define the value of the ip (localhost or VM)
 
-String? serverIp = '82.165.63.176'; // A mettre quand push github
+String? serverIp = 'risu-app.fr'; // A mettre quand push github
 
 // String? serverIp = 'localhost';
