bc1a3c0f818999dd9ea6fa42999dcebad9a3ce6a
Louismaestre <louis.maestre@epitech.eu>
2024-09-17 23:22:52 +0200

    [UPDATE] Fix failing test

--- Configurateur/front/test/components/container_test.dart
+++ Configurateur/front/test/components/container_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:front/components/container.dart';
 import 'package:front/app_routes.dart';
-import 'package:front/screens/company/container-company.dart';
 import 'package:mockito/mockito.dart';
 import 'package:http/http.dart' as http;
 import 'package:sizer/sizer.dart';
