c9dca31ae592f9d7f6a0caa94013d4a6c897a5c0
Louismaestre <louis.maestre@epitech.eu>
2024-10-15 12:09:55 +0200

    [UPDATE] Fix front test

--- Configurateur/front/test/confirmed_user_test.dart
+++ Configurateur/front/test/confirmed_user_test.dart
@@ -10,6 +10,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:front/app_routes.dart';
+import 'package:front/components/custom_footer.dart';
 import 'package:front/screens/register-confirmation/confirmed_user.dart';
 import 'package:front/services/storage_service.dart';
 import 'package:front/services/theme_service.dart';
@@ -19,14 +20,59 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sizer/sizer.dart';
 
 void main() {
-  Widget createWidgetForTesting({required Widget child}) {
-    return MaterialApp(
-      home: child,
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      locale: const Locale('fr'),
-    );
-  }
+  // Widget createWidgetForTesting({required Widget child}) {
+  //   return MaterialApp(
+  //     home: child,
+  //     localizationsDelegates: AppLocalizations.localizationsDelegates,
+  //     supportedLocales: AppLocalizations.supportedLocales,
+  //     locale: const Locale('fr'),
+  //   );
+  // }
+
+  // setUp(() async {
+  //   final roboto = rootBundle.load('assets/roboto/Roboto-Medium.ttf');
+  //   final fontLoader = FontLoader('Roboto')..addFont(roboto);
+  //   await fontLoader.load();
+  // });
+
+  // testWidgets('Confirmed user screen', (WidgetTester tester) async {
+  //   TestWidgetsFlutterBinding.ensureInitialized();
+
+  //   SharedPreferences.setMockInitialValues({});
+
+  //   storageService.writeStorage('token', 'test-token');
+  //   await tester.binding.setSurfaceSize(const Size(6000, 5000));
+
+
+  //   await tester.pumpWidget(
+  //     MultiProvider(
+  //       providers: [
+  //         ChangeNotifierProvider<ThemeService>(
+  //           create: (_) => ThemeService(),
+  //         ),
+  //       ],
+  //       child: Sizer(
+  //         builder: (context, orientation, deviceType) {
+  //           return MaterialApp(
+  //             theme: ThemeData(fontFamily: 'Roboto'),
+  //             home: InheritedGoRouter(
+  //               goRouter: AppRouter.router,
+  //               child: createWidgetForTesting(
+  //                 child: const ConfirmedUser(params: 'uuid'),
+  //               ),
+  //             ),
+  //           );
+  //         },
+  //       ),
+  //     ),
+  //   );
+
+  //   await tester.pumpAndSettle(const Duration(seconds: 2));
+
+  //   await tester.pumpAndSettle();
+  // });
+
+  TestWidgetsFlutterBinding.ensureInitialized();
 
   setUp(() async {
     final roboto = rootBundle.load('assets/roboto/Roboto-Medium.ttf');
@@ -34,14 +80,12 @@ void main() {
     await fontLoader.load();
   });
 
-  testWidgets('Confirmed user screen', (WidgetTester tester) async {
+  testWidgets('Register screen', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    tester.binding.window.physicalSizeTestValue = const Size(5000, 5000);
+    tester.binding.window.devicePixelRatioTestValue = 1.0;
+
     TestWidgetsFlutterBinding.ensureInitialized();
-
-    SharedPreferences.setMockInitialValues({});
-
-    storageService.writeStorage('token', 'test-token');
-    await tester.binding.setSurfaceSize(const Size(6000, 5000));
-
 
     await tester.pumpWidget(
       MultiProvider(
@@ -56,10 +100,11 @@ void main() {
               theme: ThemeData(fontFamily: 'Roboto'),
               home: InheritedGoRouter(
                 goRouter: AppRouter.router,
-                child: createWidgetForTesting(
-                  child: const ConfirmedUser(params: 'uuid'),
-                ),
+                child: const ConfirmedUser(params: 'uuid'),
               ),
+              localizationsDelegates: AppLocalizations.localizationsDelegates,
+              supportedLocales: AppLocalizations.supportedLocales,
+              locale: const Locale('fr'),
             );
           },
         ),
@@ -67,13 +112,14 @@ void main() {
     );
 
     await tester.pumpAndSettle(const Duration(seconds: 2));
+    expect(find.byType(CustomFooter), findsOneWidget);
 
     expect(
         find.text(
             'Votre inscription a bien été confirmée, vous pouvez maintenant vous connecter et profiter de notre application'),
         findsOneWidget);
 
-    // await tester.tap(find.byKey(const Key('go-home')));
+    await tester.tap(find.byKey(const Key('go-home')));
     await tester.pumpAndSettle();
   });
 }
