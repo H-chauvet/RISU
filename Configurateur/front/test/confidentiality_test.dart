d8a5b6fb55395e8a897872bb5bc268232e2375ea
Louismaestre <louis.maestre@epitech.eu>
2024-10-15 12:09:55 +0200

    [UPDATE] Fix front test

---
+++ Configurateur/front/test/confidentiality_test.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:front/app_routes.dart';
+import 'package:front/components/custom_footer.dart';
+import 'package:front/screens/confidentiality/confidentiality.dart';
+import 'package:front/services/theme_service.dart';
+import 'package:go_router/go_router.dart';
+import 'package:mockito/mockito.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:sizer/sizer.dart';
+
+// void main() {
+//   testWidgets('ConfidentialityPage widget test', (WidgetTester tester) async {
+//      await tester.pumpWidget(
+//       MultiProvider(
+//         providers: [
+//           ChangeNotifierProvider<ThemeService>(
+//             create: (_) => ThemeService(),
+//           ),
+//         ],
+//         child: Sizer(
+//           builder: (context, orientation, deviceType) {
+//             return MaterialApp(
+//               theme: ThemeData(fontFamily: 'Roboto'),
+//               home: InheritedGoRouter(
+//                 goRouter: AppRouter.router,
+//                 child: const ConfidentialityPage(),
+//               ),
+//               // localizationsDelegates: AppLocalizations.localizationsDelegates,
+//               // supportedLocales: AppLocalizations.supportedLocales,
+//               locale: Locale('fr'),
+//             );
+//           },
+//         ),
+//       ),
+//     );
+
+//     await tester.pump();
+    
+//   });
+// }
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() async {
+    final roboto = rootBundle.load('assets/roboto/Roboto-Medium.ttf');
+    final fontLoader = FontLoader('Roboto')..addFont(roboto);
+    await fontLoader.load();
+  });
+
+  testWidgets('Register screen', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    tester.binding.window.physicalSizeTestValue = const Size(5000, 5000);
+    tester.binding.window.devicePixelRatioTestValue = 1.0;
+
+    TestWidgetsFlutterBinding.ensureInitialized();
+
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider<ThemeService>(
+            create: (_) => ThemeService(),
+          ),
+        ],
+        child: Sizer(
+          builder: (context, orientation, deviceType) {
+            return MaterialApp(
+              theme: ThemeData(fontFamily: 'Roboto'),
+              home: InheritedGoRouter(
+                goRouter: AppRouter.router,
+                child: ConfidentialityPage(),
+              ),
+              localizationsDelegates: AppLocalizations.localizationsDelegates,
+              supportedLocales: AppLocalizations.supportedLocales,
+              locale: const Locale('fr'),
+            );
+          },
+        ),
+      ),
+    );
+
+    await tester.pumpAndSettle(const Duration(seconds: 2));
+    expect(find.byType(CustomFooter), findsOneWidget);
+
+    expect(find.text('Dernière mise à jour : 14 Octobre 2023\n\n'),
+        findsOneWidget);
+    expect(find.text('Informations que nous collectons'), findsOneWidget);
+    expect(
+        find.text('Comment nous utilisons vos informations'), findsOneWidget);
+
+    await tester.pumpAndSettle();
+  });
+}
+
+class MockSharedPreferences extends Mock implements SharedPreferences {}
+
