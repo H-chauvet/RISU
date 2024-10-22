6695fa0c54e151ee669af19e3f6ce3ce854b7937
Quentin Leroueil <quentin.leroueil@Epitech.eu>
2024-10-22 11:50:42 +0200

    [EDIT] Reduced Icon and label size in BottomNavBar

--- Application/mobile/app_code/lib/components/bottomnavbar.dart
+++ Application/mobile/app_code/lib/components/bottomnavbar.dart
@@ -69,10 +69,10 @@ class BottomNavBar extends StatelessWidget {
       backgroundColor: theme!.bottomNavigationBarTheme.backgroundColor,
       selectedItemColor: theme!.bottomNavigationBarTheme.selectedItemColor,
       unselectedItemColor: theme!.bottomNavigationBarTheme.unselectedItemColor,
-      unselectedIconTheme: const IconThemeData(size: 28),
-      selectedIconTheme: const IconThemeData(size: 28),
-      unselectedFontSize: 14,
-      selectedFontSize: 14,
+      unselectedIconTheme: const IconThemeData(size: 24),
+      selectedIconTheme: const IconThemeData(size: 24),
+      unselectedFontSize: 12,
+      selectedFontSize: 12,
       type: BottomNavigationBarType.fixed,
     );
   }
