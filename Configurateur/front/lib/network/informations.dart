ebf571725893f441554f3a2038351ae298a93dfa
tbellicha <tanguy.bellicha@epitech.eu>
2024-11-13 21:02:28 +0100

    [ADD] Firebase setup and FCM printing

--- Configurateur/front/lib/network/informations.dart
+++ Configurateur/front/lib/network/informations.dart
@@ -4,6 +4,6 @@ import 'package:flutter/foundation.dart';
 
 /// [serverIp] : Define the value of the ip (localhost or VM)
 
-String? serverIp = 'risu-app.fr'; // A mettre quand push github
+// String? serverIp = 'risu-app.fr'; // A mettre quand push github
 
-// String? serverIp = 'localhost';
+String? serverIp = 'localhost';
