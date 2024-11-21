030b77463f2451fdbb62464690b6084a1739dcce
tbellicha <tanguy.bellicha@epitech.eu>
2024-11-21 12:29:56 +0100

    [ADD] Rent Return UT

--- Application/mobile/app_code/test/rent_return_test.dart
+++ Application/mobile/app_code/test/rent_return_test.dart
@@ -96,4 +96,127 @@ void main() {
     await tester.tap(detailsButton);
     await tester.pump();
   });
+
+  testWidgets("Rental send Invoice", (WidgetTester tester) async {
+    await tester.binding.setSurfaceSize(const Size(400, 900));
+    await tester.pumpWidget(
+      initPage(
+        const ReturnArticlePage(
+          rentId: 1,
+          testRental: {
+            "id": 1,
+            "price": 100,
+            "createdAt": "2024-05-23T09:18:46.814Z",
+            "duration": 1,
+            "ended": true,
+            "item": {
+              "id": 3,
+              "name": "Ballon de football",
+              "container": {
+                "id": 1,
+                "address": "Rue d'Alger",
+                "city": "Nantes",
+              }
+            }
+          },
+        ),
+      ),
+    );
+
+    Finder sendInvoiceButton =
+        find.byKey(const Key('return_rent-button-receive_invoice'));
+    expect(sendInvoiceButton, findsOneWidget);
+
+    await tester.tap(sendInvoiceButton);
+    await tester.pump();
+  });
+
+  testWidgets("Rental getRent", (WidgetTester tester) async {
+    await tester.binding.setSurfaceSize(const Size(400, 900));
+    await tester.pumpWidget(
+      initPage(
+        const ReturnArticlePage(
+          rentId: 1,
+        ),
+      ),
+    );
+
+    await tester.pump();
+  });
+
+  testWidgets("Return rental", (WidgetTester tester) async {
+    await tester.binding.setSurfaceSize(const Size(400, 900));
+    await tester.pumpWidget(
+      initPage(
+        const ReturnArticlePage(
+          rentId: 1,
+          testRental: {
+            "id": 1,
+            "price": 100,
+            "createdAt": "2024-05-23T09:18:46.814Z",
+            "duration": 1,
+            "ended": false,
+            "item": {
+              "id": 3,
+              "name": "Ballon de football",
+              "container": {
+                "id": 1,
+                "address": "Rue d'Alger",
+                "city": "Nantes",
+              }
+            }
+          },
+        ),
+      ),
+    );
+
+    Finder returnButton =
+        find.byKey(const Key('rent_return-button-return_article'));
+    Finder alertDialogOk = find.byKey(const Key('alertdialog-button_ok'));
+    expect(returnButton, findsOneWidget);
+
+    await tester.ensureVisible(returnButton);
+
+    await tester.tap(returnButton);
+    await tester.pumpAndSettle();
+
+    expect(alertDialogOk, findsOneWidget);
+
+    await tester.tap(alertDialogOk);
+    await tester.pumpAndSettle();
+  });
+
+  testWidgets("Get direction", (WidgetTester tester) async {
+    await tester.binding.setSurfaceSize(const Size(400, 900));
+    await tester.pumpWidget(
+      initPage(
+        const ReturnArticlePage(
+          rentId: 1,
+          testRental: {
+            "id": 1,
+            "price": 100,
+            "createdAt": "2024-05-23T09:18:46.814Z",
+            "duration": 1,
+            "ended": false,
+            "item": {
+              "id": 3,
+              "name": "Ballon de football",
+              "container": {
+                "id": 1,
+                "address": "Rue d'Alger",
+                "city": "Nantes",
+              }
+            }
+          },
+        ),
+      ),
+    );
+
+    Finder getDirectionButton =
+        find.byKey(const Key('return_article-button_get_directions'));
+    expect(getDirectionButton, findsOneWidget);
+
+    await tester.tap(getDirectionButton);
+    await tester.pumpAndSettle();
+  });
 }
